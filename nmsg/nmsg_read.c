946f04003afce18d6ca1840443d65e18912b7857
Robert S. Edmonds <edmonds@debian.org>
2008-09-25 19:58:38 -0400

    clean up includes

--- nmsg/nmsg_read.c
+++ nmsg/nmsg_read.c
@@ -20,11 +20,7 @@
 
 #include <arpa/inet.h>
 #include <errno.h>
-#include <fcntl.h>
-#include <stdbool.h>
 #include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
