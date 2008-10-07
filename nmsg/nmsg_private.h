0f22b2362a648e8b0a6a4e0211fd85deb8682e61
Robert Edmonds <edmonds@isc.org>
2008-10-07 17:34:04 -0400

    basic nmsg_mod functionality, nmsgtool

--- nmsg/nmsg_private.h
+++ nmsg/nmsg_private.h
@@ -5,6 +5,9 @@
 
 #include <nmsg.h>
 
+#define ISC_CHECK_NONE 1
+#include <isc/list.h>
+
 struct nmsg_wbuf {
 	Nmsg__Nmsg *	nmsg;
 	size_t		estsz;
@@ -27,5 +30,26 @@ extern nmsg_res nmsg_buf_ensure(nmsg_buf buf, ssize_t bytes);
 extern nmsg_res nmsg_buf_fill(nmsg_buf buf);
 extern ssize_t nmsg_buf_bytes(nmsg_buf buf);
 extern ssize_t nmsg_buf_avail(nmsg_buf buf);
+
+typedef nmsg_res (*nmsg_pbmod_init)(void);
+typedef nmsg_res (*nmsg_pbmod_fini)(void);
+
+struct nmsg_pbmod {
+	int			pbmver;
+	unsigned		vendor;
+	nmsg_pbmod_init		init;
+	nmsg_pbmod_fini		fini;
+};
+
+struct nmsg_dlmod {
+	ISC_LINK(struct nmsg_dlmod)  link;
+	const char *		path;
+	void *			handle;
+};
+
+struct nmsg_pbmodset {
+	ISC_LIST(struct nmsg_dlmod)  dlmods;
+	struct nmsg_pbmod **	pbmods;
+};
 
 #endif
