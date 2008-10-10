1158d56c1665b121035a6c11e6288f326ece030a
Robert Edmonds <edmonds@isc.org>
2008-10-10 16:31:04 -0400

    nmsg_fma: finer debugging, nmsg_fma_destroy()

--- nmsg/nmsg_fma.c
+++ nmsg/nmsg_fma.c
@@ -63,16 +63,38 @@ nmsg_fma
 nmsg_fma_init(const char *name, size_t mb, unsigned debug) {
 	nmsg_fma fma;
 
-	fma = malloc(sizeof *fma);
+	fma = calloc(1, sizeof *fma);
 	assert(fma != NULL);
-	memset(fma, 0, sizeof *fma);
 	ISC_LIST_INIT(fma->block_list);
 	fma->block_size = mb * 1048576;
 	fma->debug_mode = debug;
 	fma->name = strdup(name);
 	fma->current = NULL;
+	if (fma->debug_mode >= 3)
+		fprintf(stderr, "%s: %zd MB blocksize fma init'd\n",
+			__func__, mb);
 
 	return (fma);
+}
+
+void
+nmsg_fma_destroy(nmsg_fma *pfma) {
+	struct block_ptr *block, *blocknext;
+	nmsg_fma fma = *pfma;
+
+	block = ISC_LIST_HEAD(fma->block_list);
+	while (block != NULL) {
+		blocknext = ISC_LIST_NEXT(block, link);
+		ISC_LIST_UNLINK(fma->block_list, block, link);
+		if (munmap(block->addr, fma->block_size) != 0)
+			if (fma->debug_mode >= 1)
+				perror("munmap");
+		free(block);
+		block = blocknext;
+	}
+	free(fma->name);
+	free(fma);
+	*pfma = NULL;
 }
 
 void *
@@ -84,11 +106,18 @@ nmsg_fma_alloc(nmsg_fma fma, size_t size) {
 		rec = fma_new_block(fma);
 	if ((size_t) (rec->len + size) > (size_t) fma->block_size)
 		rec = fma_new_block(fma);
-	if (rec == NULL)
+	if (rec == NULL) {
+		if (fma->debug_mode >= 3)
+			fprintf(stderr, "%s: %s %zd bytes alloc failed\n",
+				__func__, fma->name, size);
 		return (NULL);
+	}
 	oldlen = rec->len;
 	rec->num += 1;
 	rec->len += size;
+	if (fma->debug_mode >= 3)
+		fprintf(stderr, "%s: %s %zd bytes alloc'd, %u total allocs\n",
+			__func__, fma->name, size, fma->num_allocated);
 	return ((char *) rec) + oldlen;
 }
 
@@ -102,13 +131,15 @@ nmsg_fma_free(nmsg_fma fma, void *ptr) {
 	assert(rec->num >= 0);
 	if (rec->num == 0) {
 		if (munmap(rec, fma->block_size) != 0)
-			perror("munmap");
+			if (fma->debug_mode >= 1)
+				perror("munmap");
 		fma->current = NULL;
 		fma->num_allocated -= 1;
 
-		if (fma->debug_mode)
-			fprintf(stderr, "freed %s block at %p@%#.zx, %u blocks total\n",
-				fma->name, rec, fma->block_size, fma->num_allocated);
+		if (fma->debug_mode >= 4)
+			fprintf(stderr, "%s: freed %s block at %p@%#.zx, %u blocks total\n",
+				__func__, fma->name, rec, fma->block_size,
+				fma->num_allocated);
 
 		block = ISC_LIST_HEAD(fma->block_list);
 		while (block != NULL) {
@@ -121,6 +152,9 @@ nmsg_fma_free(nmsg_fma fma, void *ptr) {
 			block = blocknext;
 		}
 	}
+	if (fma->debug_mode >= 3)
+		fprintf(stderr, "%s: %s ptr freed, %u total allocs\n",
+			__func__, fma->name, fma->num_allocated);
 }
 
 /* Private. */
@@ -157,7 +191,7 @@ fma_new_block(nmsg_fma fma) {
 	ISC_LINK_INIT(block, link);
 
 	if ((block->addr = aligned_memory(fma->block_size)) == MAP_FAILED) {
-		if (fma->debug_mode) {
+		if (fma->debug_mode >= 1) {
 			perror("aligned_memory");
 			assert(0);
 		}
@@ -170,9 +204,10 @@ fma_new_block(nmsg_fma fma) {
 	rec->num = 0;
 	rec->len = sizeof(struct nmsg_fma);
 
-	if (fma->debug_mode)
-		fprintf(stderr, "allocated %s block at %p@%#.zx, %u blocks total\n",
-			fma->name, block->addr, fma->block_size, fma->num_allocated);
+	if (fma->debug_mode >= 4)
+		fprintf(stderr, "%s: allocated %s block at %p@%#.zx, %u blocks total\n",
+			__func__, fma->name, block->addr, fma->block_size,
+			fma->num_allocated);
 
 	return (rec);
 }
