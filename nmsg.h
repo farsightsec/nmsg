99d6f886d23785e6c9ef2e6d58dd2c222c35c71d
Robert Edmonds <edmonds@isc.org>
2008-10-16 19:11:53 -0400

    emailhdr: unpack / free emailhdr pbufs out of payloads

--- nmsg.h
+++ nmsg.h
@@ -27,12 +27,14 @@ typedef enum {
 	nmsg_res_memfail,
 	nmsg_res_magic_mismatch,
 	nmsg_res_version_mismatch,
+	nmsg_res_module_mismatch,
 	nmsg_res_msgsize_toolarge,
 	nmsg_res_short_send,
 	nmsg_res_wrong_buftype,
 	nmsg_res_pbuf_ready,
 	nmsg_res_notimpl,
-	nmsg_res_unknown_pbmod
+	nmsg_res_unknown_pbmod,
+	nmsg_res_no_payload
 } nmsg_res;
 
 typedef enum {
