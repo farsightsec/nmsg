70bf7a2c09750cf02f7ea0d912d08b94309d9229
Robert Edmonds <edmonds@isc.org>
2009-02-06 18:36:40 -0500

    pbnmsg_isc_http header file

---
+++ nmsg/isc/pbnmsg_isc_http.h
@@ -0,0 +1,14 @@
+#ifndef PBNMSG_ISC_HTTP_H
+#define PBNMSG_ISC_HTTP_H
+
+enum pbnmsg_isc_http_fields {
+	pbnmsg_isc_http_f_type,
+	pbnmsg_isc_http_f_srcip,
+	pbnmsg_isc_http_f_srchost,
+	pbnmsg_isc_http_f_dstip,
+	pbnmsg_isc_http_f_dstport,
+	pbnmsg_isc_http_f_request,
+	pbnmsg_isc_http_f_p0f
+};
+
+#endif /* PBNMSG_ISC_HTTP_H */
