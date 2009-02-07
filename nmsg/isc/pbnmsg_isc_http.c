786e6f489786fd2c367a2a1eb10105fb881a3d13
Robert Edmonds <edmonds@isc.org>
2009-02-07 02:26:20 -0500

    pbnmsg_isc_http: pare down to the new pbnmsg api

--- nmsg/isc/pbnmsg_isc_http.c
+++ nmsg/isc/pbnmsg_isc_http.c
@@ -35,40 +35,26 @@
 
 #include "http.pb-c.h"
 
-/* Data structures. */
-
-struct http_clos {
-	Nmsg__Isc__Http		*h;
-	size_t			size;
-};
-
-struct http_type {
-	Nmsg__Isc__HttpType	type;
-	const char		*name;
-};
-
 /* Data. */
 
-struct http_type module_http_types[] = {
-	{ NMSG__ISC__HTTP_TYPE__unknown,	"unknown"	},
-	{ NMSG__ISC__HTTP_TYPE__sinkhole,	"sinkhole"	},
+#define descrs		nmsg__isc__http__field_descriptors
+extern const ProtobufCFieldDescriptor descrs[];
+struct nmsg_pbmod_field http_fields[] = {
+	{ nmsg_pbmod_ft_enum,			&descrs[0] }, /* type */
+	{ nmsg_pbmod_ft_ip,			&descrs[1] }, /* srcip */
+	{ nmsg_pbmod_ft_string,			&descrs[2] }, /* srchost */
+	{ nmsg_pbmod_ft_uint16,			&descrs[3] }, /* srcport */
+	{ nmsg_pbmod_ft_ip,			&descrs[4] }, /* dstip */
+	{ nmsg_pbmod_ft_uint16,			&descrs[5] }, /* dstport */
+	{ nmsg_pbmod_ft_multiline_string,	&descrs[6] }, /* request */
+	{ nmsg_pbmod_ft_multiline_string,	&descrs[7] }, /* p0f */
 	{ 0, NULL }
 };
 
-/* Forward. */
-
-static const char *http_type_to_str(Nmsg__Isc__HttpType);
-static nmsg_res finalize_pbuf(struct http_clos *, uint8_t **pbuf, size_t *);
-
 /* Exported via module context. */
 
-static void *http_init(int debug);
-static nmsg_res http_fini(void *);
 static nmsg_res http_pbuf_to_pres(Nmsg__NmsgPayload *, char **pres,
 				  const char *endline);
-static nmsg_res http_field_to_pbuf(void *, const char *field,
-				   const uint8_t *val, size_t len,
-				   uint8_t **pbuf, size_t *);
 
 /* Macros. */
 
@@ -79,10 +65,9 @@ static nmsg_res http_field_to_pbuf(void *, const char *field,
 
 struct nmsg_pbmod nmsg_pbmod_ctx = {
 	.pbmver = NMSG_PBMOD_VERSION,
-	.init = &http_init,
-	.fini = &http_fini,
 	.pbuf2pres = &http_pbuf_to_pres,
-	.field2pbuf = &http_field_to_pbuf,
+	.descr = &nmsg__isc__http__descriptor,
+	.fields = http_fields,
 	.vendor = NMSG_VENDOR_ISC,
 	.msgtype = {
 		{ MSGTYPE_HTTP_ID, MSGTYPE_HTTP_NAME },
@@ -91,22 +76,6 @@ struct nmsg_pbmod nmsg_pbmod_ctx = {
 };
 
 /* Exported via module context. */
-
-static void *
-http_init(int debug) {
-	struct http_clos *clos;
-	if (debug > 2)
-		fprintf(stderr, "http: starting module\n");
-
-	clos = calloc(1, sizeof(*clos));
-	return (clos);
-}
-
-static nmsg_res
-http_fini(void *clos) {
-	free(clos);
-	return (nmsg_res_success);
-}
 
 static nmsg_res
 http_pbuf_to_pres(Nmsg__NmsgPayload *np, char **pres, const char *el) {
@@ -164,7 +133,8 @@ http_pbuf_to_pres(Nmsg__NmsgPayload *np, char **pres, const char *el) {
 		      "%s%s%s%s%s"	/* p0f results */
 		      "\n"
 		      ,
-		      http_type_to_str(http->type), el,
+		      /* http_type_to_str(http->type), el, */
+		      "DUNNO", el,
 
 		      http->has_srchost	? "srchost: " : "",
 		      http->has_srchost	? (char *) http->srchost.data : "",
@@ -199,114 +169,3 @@ http_pbuf_to_pres(Nmsg__NmsgPayload *np, char **pres, const char *el) {
 	free(dstport);
 	return (nmsg_res_success);
 }
-
-static nmsg_res
-http_field_to_pbuf(void *cl, const char *field, const uint8_t *val,
-		       size_t len, uint8_t **pbuf, size_t *sz)
-{
-	struct http_clos *clos;
-	Nmsg__Isc__Http *h;
-
-	clos = (struct http_clos *) cl;
-	if (clos->h == NULL) {
-		clos->h = calloc(1, sizeof(*clos->h));
-		if (clos->h == NULL)
-			return (nmsg_res_memfail);
-		clos->h->base.descriptor = &nmsg__isc__http__descriptor;
-	}
-	h = clos->h;
-
-	if (pbuf != NULL && sz != NULL)
-		return (finalize_pbuf(clos, pbuf, sz));
-
-	if (strcmp(field, "type") == 0) {
-		if (strcmp((char *) val, "sinkhole") == 0)
-			h->type = NMSG__ISC__HTTP_TYPE__sinkhole;
-		else
-			assert(0);
-		clos->size += 1;
-	} else if (strcmp(field, "srcip") == 0) {
-		if (len == 4 || len == 16) {
-			h->srcip.data = malloc(len);
-			if (h->srcip.data == NULL)
-				return (nmsg_res_memfail);
-			memcpy(h->srcip.data, val, len);
-			h->srcip.len = len;
-			clos->size += len;
-			h->has_srcip = true;
-		}
-	} else if (strcmp(field, "dstip") == 0) {
-		if (len == 4 || len == 16) {
-			h->dstip.data = malloc(len);
-			if (h->dstip.data == NULL)
-				return (nmsg_res_memfail);
-			memcpy(h->dstip.data, val, len);
-			h->dstip.len = len;
-			clos->size += len;
-			h->has_dstip = true;
-		}
-	} else if (strcmp(field, "srchost") == 0) {
-		h->srchost.data = malloc(len);
-		if (h->srchost.data == NULL)
-			return (nmsg_res_memfail);
-		memcpy(h->srchost.data, val, len);
-		h->srchost.len = len;
-		clos->size += len;
-		h->has_srchost = true;
-	} else if (strcmp(field, "srcport") == 0) {
-		h->srcport = *((uint16_t *) val);
-		h->has_srcport = true;
-		clos->size += 2;
-	} else if (strcmp(field, "dstport") == 0) {
-		h->dstport = *((uint16_t *) val);
-		h->has_dstport = true;
-		clos->size += 2;
-	} else if (strcmp(field, "request") == 0) {
-		h->request.data = malloc(len);
-		if (h->request.data == NULL)
-			return (nmsg_res_memfail);
-		memcpy(h->request.data, val, len);
-		h->request.len = len;
-		clos->size += len;
-		h->has_request = true;
-	}
-
-	return (nmsg_res_success);
-}
-
-
-/* Private. */
-
-static const char *
-http_type_to_str(Nmsg__Isc__HttpType type) {
-	struct http_type *ht;
-
-	for (ht = module_http_types;
-	     ht->name != NULL;
-	     ht++)
-	{
-		if (type == ht->type)
-			return (ht->name);
-	}
-	return ("UNKNOWN");
-}
-
-
-static nmsg_res
-finalize_pbuf(struct http_clos *clos, uint8_t **pbuf, size_t *sz) {
-	*pbuf = malloc(2 * clos->size);
-	if (*pbuf == NULL) {
-		free(clos->h);
-		return (nmsg_res_memfail);
-	}
-	*sz = nmsg__isc__http__pack(clos->h, *pbuf);
-	free(clos->h->srcip.data);
-	free(clos->h->dstip.data);
-	free(clos->h->srchost.data);
-	free(clos->h->request.data);
-	free(clos->h);
-	clos->h = NULL;
-	clos->size = 0;
-	return (nmsg_res_pbuf_ready);
-}
-
