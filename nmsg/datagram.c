a66ca7b523db4b163b81a3867e79984f7e9792f2
Robert Edmonds <edmonds@isc.org>
2009-02-27 18:52:15 -0500

    datagram: initial import

---
+++ nmsg/datagram.c
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2009 by Internet Systems Consortium, Inc. ("ISC")
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/* Import. */
+
+#include "nmsg_port.h"
+#include "nmsg_port_net.h"
+
+#include <pcap.h>
+
+#include "datagram.h"
+#include "res.h"
+
+/* Macros. */
+#define advance_pkt(pkt, len, sz) do { \
+	(pkt) += (sz); \
+	(len) -= (sz); \
+} while (0)
+
+/* Export. */
+
+nmsg_res
+nmsg_datagram_find_network(struct nmsg_datagram *dg, int datalink,
+			   const u_char *pkt, size_t len)
+{
+	unsigned etype;
+	nmsg_res res = nmsg_res_failure;
+
+	if (datalink == DLT_EN10MB && len >= ETHER_HDR_LEN) {
+		const struct ether_header *eth;
+
+		eth = (const struct ether_header *) pkt;
+		advance_pkt(pkt, len, ETHER_HDR_LEN);
+		etype = ntohs(eth->ether_type);
+		if (etype == ETHERTYPE_VLAN && len >= 4) {
+			advance_pkt(pkt, len, 2);
+			etype = ntohs(*(const uint16_t *) pkt);
+			advance_pkt(pkt, len, 2);
+		}
+		res = nmsg_res_success;
+	}
+#ifdef DLT_LINUX_SLL
+	else if (datalink == DLT_LINUX_SLL && len >= 16) {
+		advance_pkt(pkt, len, ETHER_HDR_LEN);
+		etype = ntohs(*(const uint16_t *) pkt);
+		advance_pkt(pkt, len, 2);
+		res = nmsg_res_success;
+	}
+#endif
+	if (res == nmsg_res_success &&
+	    (etype == ETHERTYPE_IP || etype == ETHERTYPE_IPV6))
+	{
+		dg->network = pkt;
+		dg->len_network = len;
+		dg->proto_network = etype;
+	} else {
+		res = nmsg_res_failure;
+	}
+
+	return (res);
+}
+
+bool
+nmsg_datagram_is_fragment(struct nmsg_datagram *dg) {
+	const struct *ip;
+
+	if (dg->proto_network == ETHERTYPE_IP) {
+		unsigned ip_off;
+
+		ip = (const struct ip *) dg->network;
+		ip_off = ntohs(ip->ip_off);
+		if ((ip_off & IP_OFFMASK) != 0 ||
+		    (ip_off & IP_MF) != 0)
+		{
+			return (true);
+		}
+
+	} else if (dg->proto_network == ETHERTYPE_IPV6) {
+		/* XXX */
+	}
+	return (false);
+}
+
+nmsg_res
+nmsg_datagram_find_transport(struct nmsg_datagram *dg) {
+	const u_char *pkt;
+	unsigned len;
+	nmsg_res res;
+
+	pkt = dg->network;
+	len = dg->len_network;
+	res = nmsg_res_failure;
+
+	if (dg->proto_network == ETHERTYPE_IP) {
+		const struct ip *ip;
+
+		if (dg->len_network >= sizeof(*ip)) {
+			ip = (const struct ip *) dg->network;
+			if (ip->ip_v == IPVERSION) {
+				if (len >= ip->ip_hl * 4U) {
+					advance_pkt(pkt, len, ip->ip_hl * 4U);
+					dg->transport = pkt;
+					dg->len_transport = len;
+					dg->proto_transport = ip->ip_p;
+					res = nmsg_res_success;
+				}
+			}
+		}
+	} else if (dg->proto_network == ETHERTYPE_IPV6) {
+		/* XXX */
+	}
+
+	return (res);
+}
---
+++ nmsg/datagram.h
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2009 by Internet Systems Consortium, Inc. ("ISC")
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef NMSG_DATAGRAM_H
+#define NMSG_DATAGRAM_H
+
+/*****
+ ***** Module Info
+ *****/
+
+/*! \file nmsg/datagram.h
+ * \brief Datagram utility functions.
+ */
+
+/***
+ *** Imports
+ ***/
+
+#include <sys/types.h>
+
+#include <nmsg/res.h>
+
+/***
+ *** Types
+ ***/
+
+struct nmsg_datagram {
+	int		proto_network;
+	int		proto_transport;
+	unsigned	len_network;
+	unsigned	len_transport;
+	unsigned	len_payload;
+	const u_char	*network;
+	const u_char	*transport;
+	const u_char	*payload;
+};
+
+/***
+ *** Functions
+ ***/
+
+nmsg_res
+nmsg_datagram_find_network(struct nmsg_datagram *dg, int datalink,
+			   const u_char *pkt, size_t len);
+/*%<
+ * Find the network header of an IP datagram and populate a struct
+ * nmsg_datagram.
+ *
+ * Requires:
+ *
+ * \li	'dg' is a caller-allocated struct nmsg_datagram.
+ *
+ * \li	'datalink' is a valid libpcap datalink type. Supported datalink
+ *	types are DLT_EN10MB and DLT_LINUX_SLL.
+ *
+ * \li	'pkt' is a pointer to the packet. The packet must match the
+ *	specified datalink type.
+ *
+ * \li	'len' is the total number of octets captured in 'pkt'.
+ *
+ * Returns:
+ *
+ * \li	nmsg_res_success
+ * \li	nmsg_res_failure
+ */
+
+nmsg_res
+nmsg_datagram_find_transport(struct nmsg_datagram *dg);
+
+#endif /* NMSG_DATAGRAM_H */
