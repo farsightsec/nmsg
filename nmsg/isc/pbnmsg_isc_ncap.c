67373a468ba743a3204358144db4e5115b2b046f
Robert Edmonds <edmonds@isc.org>
2009-03-05 22:38:52 -0500

    pbnmsg_isc_ncap: stubs

--- nmsg/isc/pbnmsg_isc_ncap.c
+++ nmsg/isc/pbnmsg_isc_ncap.c
@@ -1,7 +1,7 @@
-/* pbnmsg_isc_ncap.c - protobuf nmsg module implementing ISC NCAP */
+/* pbnmsg_isc_ncap.c - ncap protobuf nmsg module */
 
 /*
- * Copyright (c) 2008 by Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (c) 2009 by Internet Systems Consortium, Inc. ("ISC")
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -18,7 +18,30 @@
 
 /* Import. */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <nmsg.h>
+#include <nmsg/ipdg.h>
+#include <nmsg/pbmod.h>
+
+#include "pbnmsg_isc_ncap.h"
+#include "ncap.pb-c.c"
+
+/* Data structures. */
+
+struct ncap_clos {
+	int	debug;
+};
+
+/* Exported via module context. */
+
+static nmsg_res ncap_init(void **clos, int debug);
+static nmsg_res ncap_fini(void **clos);
+static nmsg_res ncap_pbuf_to_pres(Nmsg__NmsgPayload *np, char **pres,
+				  const char *el);
+static nmsg_res ncap_ipdg_to_pbuf(void *clos, const struct nmsg_ipdg *dg,
+				  uint8_t **pbuf, size_t *sz);
 
 /* Export. */
 
@@ -26,6 +49,52 @@ struct nmsg_pbmod nmsg_pbmod_ctx = {
 	.pbmver = NMSG_PBMOD_VERSION,
 	.vendor = NMSG_VENDOR_ISC,
 	.msgtype = {
-		{ 1, "ncap" }
-	}
+		{ MSGTYPE_NCAP_ID, MSGTYPE_NCAP_NAME },
+		NMSG_IDNAME_END
+	},
+	.init = ncap_init,
+	.fini = ncap_fini,
+	.pbuf_to_pres = ncap_pbuf_to_pres,
+	.ipdg_to_pbuf = ncap_ipdg_to_pbuf
 };
+
+/* Private. */
+
+static nmsg_res
+ncap_init(void **clos, int debug) {
+	struct ncap_clos *nclos;
+
+	nclos = *clos = calloc(1, sizeof(*nclos));
+	if (nclos == NULL)
+		return (nmsg_res_memfail);
+	nclos->debug = debug;
+
+	return (nmsg_res_success);
+}
+
+static nmsg_res
+ncap_fini(void **clos) {
+	free(*clos);
+	*clos = NULL;
+	return (nmsg_res_success);
+}
+
+static nmsg_res
+ncap_pbuf_to_pres(Nmsg__NmsgPayload *np __attribute__((unused)),
+		  char **pres __attribute__((unused)),
+		  const char *el __attribute__((unused)))
+{
+	return (nmsg_res_success);
+}
+
+static nmsg_res
+ncap_ipdg_to_pbuf(void *clos, const struct nmsg_ipdg *dg,
+		  uint8_t **pbuf, size_t *sz)
+{
+	struct ncap_clos *nclos = (struct ncap_clos *) clos;
+
+	fprintf(stderr, "%s: nclos=%p dg=%p pbuf=%p sz=%p\n",
+		__func__, nclos, dg, pbuf, sz);
+
+	return (nmsg_res_success);
+}
---
+++ nmsg/isc/pbnmsg_isc_ncap.h
@@ -0,0 +1,9 @@
+#ifndef PBNMSG_ISC_NCAP_H
+#define PBNMSG_ISC_NCAP_H
+
+#include "ncap.pb-c.h"
+
+#define MSGTYPE_NCAP_ID		1
+#define MSGTYPE_NCAP_NAME	"ncap"
+
+#endif /* PBNMSG_ISC_NCAP_H */
