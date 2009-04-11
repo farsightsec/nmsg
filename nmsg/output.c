3b5a4a5160a7e5719f888f1627740260aa6fc799
Robert Edmonds <edmonds@isc.org>
2009-04-10 23:22:12 -0400

    output: have nmsg_output_set_buffered() handle line buffering for pres outputs

--- nmsg/output.c
+++ nmsg/output.c
@@ -223,8 +223,16 @@ nmsg_output_close(nmsg_output_t *output) {
 
 void
 nmsg_output_set_buffered(nmsg_output_t output, bool buffered) {
-	assert(output->stream->type == nmsg_stream_type_sock);
-	output->stream->buffered = buffered;
+	if (output->type == nmsg_output_type_stream &&
+	    output->stream->type == nmsg_stream_type_sock)
+	{
+		output->stream->buffered = buffered;
+	} else if (output->type == nmsg_output_type_pres) {
+		if (buffered == true)
+			setvbuf(output->pres->fp, NULL, _IOLBF, 0);
+		else
+			setvbuf(output->pres->fp, NULL, _IOFBF, 0);
+	}
 }
 
 void
