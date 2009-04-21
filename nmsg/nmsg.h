4d75bdc6a34414430cc030dfd9aef7c73b4d0c8a
Robert Edmonds <edmonds@isc.org>
2009-04-21 18:57:02 -0400

    nmsg.h: add nmsg_strbuf_t type

--- nmsg/nmsg.h
+++ nmsg/nmsg.h
@@ -49,6 +49,7 @@ typedef struct nmsg_pcap *	nmsg_pcap_t;
 typedef struct nmsg_pres *	nmsg_pres_t;
 typedef struct nmsg_rate *	nmsg_rate_t;
 typedef struct nmsg_reasm_ip *	nmsg_ipreasm_t;
+typedef struct nmsg_strbuf *	nmsg_strbuf_t;
 typedef struct nmsg_zbuf *	nmsg_zbuf_t;
 
 struct nmsg_idname {
