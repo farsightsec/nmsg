c15edafe0d7fa13f0d6b9f13374ac541d2891106
Robert Edmonds <edmonds@isc.org>
2009-04-24 20:17:39 -0400

    nmsgtool: clean up header imports

--- src/nmsgtool.c
+++ src/nmsgtool.c
@@ -18,23 +18,10 @@
 
 #include "nmsg_port.h"
 
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-
 #include <assert.h>
-#include <errno.h>
-#include <fcntl.h>
 #include <signal.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
 
 #include <nmsg.h>
 
