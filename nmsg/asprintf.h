92f7db1bb8319e414cc9218d9fb399288ee98600
Robert Edmonds <edmonds@isc.org>
2009-05-08 00:41:54 -0400

    asprintf: doxygen

--- nmsg/asprintf.h
+++ nmsg/asprintf.h
@@ -1,8 +1,14 @@
 #ifndef NMSG_ASPRINTF_H
 
+/*! \file nmsg/asprintf.h
+ * \brief Asprintf utility functions.
+ *
+ * Portable replacements for the asprintf(3) and vasprintf(3) functions.
+ */
+
 #include <stdarg.h>
 
-int nmsg_asprintf(char **, const char *, ...);
-int nmsg_vasprintf(char **, const char *, va_list);
+int nmsg_asprintf(char **strp, const char *fmt, ...);
+int nmsg_vasprintf(char **strp, const char *fmt, va_list args);
 
 #endif /* NMSG_ASPRINTF_H */
