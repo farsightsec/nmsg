4697508e238920647b15c7312320f79e43f04a8e
Robert Edmonds <edmonds@isc.org>
2009-05-08 01:43:38 -0400

    payload: doxygen

--- nmsg/payload.h
+++ nmsg/payload.h
@@ -17,159 +17,119 @@
 #ifndef NMSG_PAYLOAD_H
 #define NMSG_PAYLOAD_H
 
-/*****
- ***** Module Info
- *****/
-
 /*! \file nmsg/payload.h
  * \brief Utility functions for manipulating nmsg payloads.
  */
-
-/***
- *** Imports
- ***/
 
 #include <time.h>
 
 #include <nmsg.h>
 
-/***
- *** Functions
- ***/
-
-Nmsg__NmsgPayload *nmsg_payload_dup(const Nmsg__NmsgPayload *np);
-/*%<
+/**
  * Duplicate an nmsg payload.
  *
- * Requires:
+ * \param[in] np nmsg payload
  *
- * \li	'np' is a valid nmsg payload.
- *
- * \li	'ca' is an allocator object in which the 'alloc' field must be set,
- *	and the 'allocator_data' field may optionally be set.
- *
- * Returns:
- *
- * \li	A copy of the 'np' argument which must be freed by the caller.
+ * \return Copy of the payload which must be freed by the caller using
+ *	nmsg_payload_free().
  */
+Nmsg__NmsgPayload *nmsg_payload_dup(const Nmsg__NmsgPayload *np);
 
+/**
+ * Free an nmsg payload.
+ *
+ * \param[in] np pointer to an nmsg payload
+ */
 void
 nmsg_payload_free(Nmsg__NmsgPayload **np);
-/*%<
- * Free an nmsg payload allocated by nmsg_payload_dup().
- *
- * \li	'*np' is a valid nmsg payload.
- *
- * Ensures:
- *
- * \li	*np is NULLed and memory used by the nmsg payload and its
- *	serialized data are freed.
- */
 
+/**
+ * Determine the serialized length of an nmsg payload.
+ *
+ * \param[in] np nmsg payload
+ *
+ * \return Length in octets the payload will consume after serialization.
+ */
 size_t
 nmsg_payload_size(const Nmsg__NmsgPayload *np);
-/*%<
- * Determine the length of a serialized nmsg payload.
- *
- * Requires:
- *
- * \li	'np' is a valid nmsg payload.
- *
- * Returns:
- *
- * \li	Length (in octets) the payload will consume after serialization.
- */
 
+/**
+ * Create an nmsg payload.
+ *
+ * \param[in] pbuf serialized data.
+ *
+ * \param[in] sz length of the serialized data.
+ *
+ * \param[in] vid vendor ID of the module which generated the serialized data.
+ *
+ * \param[in] msgtype message type of the module which generated the serialized
+ *	data.
+ *
+ * \param[in] ts timestamp to embed in the nmsg payload.
+ *
+ * \return nmsg payload.
+ */
 Nmsg__NmsgPayload *
 nmsg_payload_make(uint8_t *pbuf, size_t sz, unsigned vid, unsigned msgtype,
 		  const struct timespec *ts);
-/*%<
- * Create an nmsg payload.
+
+/**
+ * Create an nmsg payload from a protobuf message.
  *
- * Requires:
+ * \param[in] m initialized protobuf message ready to be serialized.
  *
- * \li	'pbuf' is serialized data.
+ * \param[in] vid vendor ID.
  *
- * \li	'sz' is the length of the serialized data.
+ * \param[in] msgtype message type.
  *
- * \li	'vid' is the vendor ID of the module which generated the serialized
- *	data.
+ * \param[in] ts timestamp to embed in the nmsg payload.
  *
- * \li	'msgtype' is the message type of the module which generated the
- *	serialized data.
- *
- * \li	'ts' is the timestamp to embed in the nmsg payload.
- *
- * Returns:
- *
- * \li	An nmsg payload.
+ * \return nmsg payload.
  */
-
-
 Nmsg__NmsgPayload *
 nmsg_payload_from_message(void *m, unsigned vid, unsigned msgtype,
 			  const struct timespec *ts);
-/*%<
- * Create an nmsg payload from a protobuf message.
- *
- * Requires:
- *
- * \li	'm' is an initialized protobuf message ready to be serialized.
- *
- * \li	'vid' is a vendor ID.
- *
- * \li	'msgtype' is a message type.
- *
- * \li	'ts' is the timestamp to embed in the nmsg payload.
- *
- * Returns:
- * \li	An nmsg payload.
- */
 
+/**
+ * Load a byte array nmsg payload field with an IP address converted from
+ * presentation format.
+ *
+ * \param[in] has protobuf quantifier field. In protobuf struct definitions, a
+ *	field prefixed with has_ or n_ accompanies optional or repeated fields.
+ *	May be NULL if the field is required.
+ *
+ * \param[out] bdata protobuf byte array field where the IP address should be
+ *	stored.
+ *
+ * \param[in] af address family, AF_INET or AF_INET6.
+ *
+ * \param[in] src presentation form of an IP address.
+ *
+ * \return #nmsg_res_success
+ * \return #nmsg_res_memfail
+ * \return #nmsg_res_failure
+ */
 nmsg_res
 nmsg_payload_put_ipstr(ProtobufCBinaryData *bdata, int *has, int af,
 		       const char *src);
-/*%<
- * Load a byte array payload field with an IP address converted from
- * presentation format.
- *
- * \li	'has' is the protobuf quantifier field. In protobuf struct
- *	definitions, a field prefixed with has_ or n_ accompanies optional
- *	or repeated fields.
- *
- * \li	'bdata' is the protobuf byte array field where the IP address
- *	should be stored.
- *
- * \li	'af' is an address family, AF_INET or AF_INET6.
- *
- * \li	'src' is the presentation form of an IP address.
- *
- * Returns:
- *
- * \li	nmsg_res_success
- * \li	nmsg_res_memfail
- * \li	nmsg_res_failure
- */
 
-nmsg_res
-nmsg_payload_put_str(ProtobufCBinaryData *bdata, int *has, const char *str);
-/*%<
- * Load a byte array payload field with a string. The string is copied with
+/**
+ * Load a byte array nmsg payload field with a string. The string is copied with
  * strdup().
  *
- * \li	'has' is the protobuf quantifier field. In protobuf struct
- *	definitions, a field prefixed with has_ or n_ accompanies optional
- *	or repeated fields.
+ * \param[in] has protobuf quantifier field. In protobuf struct definitions, a
+ *	field prefixed with has_ or n_ accompanies optional or repeated fields.
+ *	May be NULL if the field is required.
  *
- * \li	'bdata' is the protobuf byte array field where the IP address
- *	should be stored.
+ * \param[out] bdata is the protobuf byte array field where the string should be
+ *	stored.
  *
- * \li	'str' is the \0 terminated string to copy.
+ * \param[in] str \\0 terminated string to copy.
  *
- * Returns:
- *
- * \li	nmsg_res_success
- * \li	nmsg_res_memfail
+ * \return #nmsg_res_success
+ * \return #nmsg_res_memfail
  */
+nmsg_res
+nmsg_payload_put_str(ProtobufCBinaryData *bdata, int *has, const char *str);
 
 #endif /* NMSG_PAYLOAD_H */
