41f85d83a042705e2569526321ba0457088409d1
Robert Edmonds <edmonds@isc.org>
2009-05-08 00:58:59 -0400

    ipdg: doxygen

--- nmsg/ipdg.h
+++ nmsg/ipdg.h
@@ -17,47 +17,30 @@
 #ifndef NMSG_IPDG_H
 #define NMSG_IPDG_H
 
-/*****
- ***** Module Info
- *****/
-
 /*! \file nmsg/ipdg.h
- * \brief IP datagram utility functions.
+ * \brief IP datagram parsing functions.
  */
-
-/***
- *** Imports
- ***/
 
 #include <sys/types.h>
 
 #include <pcap.h>
-
 #include <nmsg.h>
 
-/***
- *** Types
- ***/
-
+/**
+ * Parsed IP datagram.
+ */
 struct nmsg_ipdg {
-	int		proto_network;
-	int		proto_transport;
-	unsigned	len_network;
-	unsigned	len_transport;
-	unsigned	len_payload;
-	const u_char	*network;
-	const u_char	*transport;
-	const u_char	*payload;
+	int		proto_network;	 /*%< PF_* value */
+	int		proto_transport; /*%< transport protocol */
+	unsigned	len_network;	 /*%< length starting from network */
+	unsigned	len_transport;	 /*%< length starting from transport */
+	unsigned	len_payload;	 /*%< length starting from payload */
+	const u_char	*network;	 /*%< pointer to network header */
+	const u_char	*transport;	 /*%< pointer to transport header */
+	const u_char	*payload;	 /*%< pointer to application payload */
 };
 
-/***
- *** Functions
- ***/
-
-nmsg_res
-nmsg_ipdg_parse(struct nmsg_ipdg *dg, unsigned etype, size_t len,
-		const u_char *pkt);
-/*%<
+/**
  * Parse an IP datagram and populate a struct nmsg_ipdg indicating where
  * the network, transport, and payload sections of the datagram are and the
  * length of the remaining packet at each of those sections.
@@ -66,28 +49,24 @@ nmsg_ipdg_parse(struct nmsg_ipdg *dg, unsigned etype, size_t len,
  *
  * Broken and fragmented datagrams are discarded.
  *
- * Requires:
- *
- * \li	'dg' is a caller-allocated struct nmsg_ipdg which will be populated
+ * \param[out] dg caller-allocated struct nmsg_ipdg which will be populated
  *	after a successful call.
  *
- * \li	'etype' is an ETHERTYPE_* value. The only supported values are
+ * \param[in] etype ETHERTYPE_* value. The only supported values are
  *	ETHERTYPE_IP and ETHERTYPE_IPV6.
  *
- * \li	'len' is the length of the packet.
+ * \param[in] len length of the packet.
  *
- * \li	'pkt' is a pointer to the packet.
+ * \param[in] pkt pointer to the packet.
  *
- * Returns:
- *
- * \li	nmsg_res_success
- * \li	nmsg_res_again
+ * \return #nmsg_res_success
+ * \return #nmsg_res_again
  */
-
 nmsg_res
-nmsg_ipdg_parse_pcap(struct nmsg_ipdg *dg, nmsg_pcap_t pcap,
-		     struct pcap_pkthdr *pkt_hdr, const u_char *pkt);
-/*%<
+nmsg_ipdg_parse(struct nmsg_ipdg *dg, unsigned etype, size_t len,
+		const u_char *pkt);
+
+/**
  * Parse an IP datagram and populate a struct nmsg_ipdg indicating where
  * the network, transport, and payload sections of the datagram are and the
  * length of the remaining packet at each of those sections.
@@ -97,33 +76,26 @@ nmsg_ipdg_parse_pcap(struct nmsg_ipdg *dg, nmsg_pcap_t pcap,
  * associated with 'pcap' and must be pkt_hdr->caplen octets long.
  *
  * Broken packets are discarded. All but the final fragment of a fragmented
- * datagram are stored internally and nmsg_res_again is returned.
+ * datagram are stored internally and #nmsg_res_again is returned.
  *
- * Requires:
- *
- * \li	'dg' is a caller-allocated struct nmsg_ipdg which will be populated
+ * \param[out] dg caller-allocated struct nmsg_ipdg which will be populated
  *	after a successful call.
  *
- * \li	'pcap' is a caller-initialized nmsg_pcap object from whose pcap
- *	handle the packet 'pkt' was received.
+ * \param[in] pcap caller-initialized nmsg_pcap object from whose pcap handle
+ *	the packet 'pkt' was received.
  *
- * \li	'pkt_hdr' is a pointer to the pcap packet header corresponding
- *	to 'pkt'.
+ * \param[in] pkt_hdr pointer to the pcap packet header corresponding to 'pkt'.
  *
- * \li	'pkt' is a pointer to the packet.
+ * \param[in] pkt pointer to the packet.
  *
- * Returns:
- *
- * \li	nmsg_res_success
- * \li	nmsg_res_again
+ * \return #nmsg_res_success
+ * \return #nmsg_res_again
  */
-
 nmsg_res
-nmsg_ipdg_parse_reasm(struct nmsg_ipdg *dg, unsigned etype, size_t len,
-		      const u_char *pkt, nmsg_ipreasm_t reasm,
-		      unsigned *new_len, u_char *new_pkt, int *defrag,
-		      uint64_t timestamp);
-/*%<
+nmsg_ipdg_parse_pcap(struct nmsg_ipdg *dg, nmsg_pcap_t pcap,
+		     struct pcap_pkthdr *pkt_hdr, const u_char *pkt);
+
+/**
  * Parse an IP datagram and populate a struct nmsg_ipdg indicating where
  * the network, transport, and payload sections of the datagram are and the
  * length of the remaining packet at each of those sections.
@@ -131,42 +103,43 @@ nmsg_ipdg_parse_reasm(struct nmsg_ipdg *dg, unsigned etype, size_t len,
  * This function operates on datagrams from the network layer.
  *
  * Broken packets are discarded. All but the final fragment of a fragmented
- * datagram are stored internally and nmsg_res_again is returned.
+ * datagram are stored internally and #nmsg_res_again is returned.
  *
  * Calling this function with the last four parameters set to NULL or 0 is
  * equivalent to calling nmsg_ipdg_parse().
  *
- * Requires:
- *
- * \li	'dg' is a caller-allocated struct nmsg_ipdg which will be populated
+ * \param[out] dg caller-allocated struct nmsg_ipdg which will be populated
  *	after a successful call.
  *
- * \li	'etype' is an ETHERTYPE_* value. The only supported values are
+ * \param[in] etype ETHERTYPE_* value. The only supported values are
  *	ETHERTYPE_IP and ETHERTYPE_IPV6.
  *
- * \li	'len' is the length of the packet.
+ * \param[in] len length of the packet.
  *
- * \li	'pkt' is a pointer to the packet.
+ * \param[in] pkt pointer to the packet.
  *
- * \li	'reasm' is a caller-initialized struct nmsg_ipreasm object.
+ * \param[in] reasm caller-initialized struct nmsg_ipreasm object.
  *
- * \li	'new_len' is a parameter-return value indicating the length of
- *	'new_pkt'. If IP reassembly is performed, its value after return
- *	is the length of the reassembled IP datagram stored in 'new_pkt'.
+ * \param[in,out] new_len length of 'new_pkt'. If IP reassembly is performed,
+ *	its value after return is the length of the reassembled IP datagram
+ *	stored in 'new_pkt'.
  *
- * \li	'new_pkt' is a buffer of at least '*new_len' bytes where a
+ * \param[out] new_pkt buffer of at least '*new_len' bytes where a
  *	reassembled IP datagram will be stored if reassembly is performed.
  *
- * \li	'timestamp' is an arbitrary timestamp, such as seconds since the
- *	unix epoch.
+ * \param[in] 'timestamp' arbitrary timestamp, such as seconds since the unix
+ *	epoch.
  *
- * \li	'defrag' is NULL, or a pointer to where the value 1 will be stored if
+ * \param[out] defrag NULL, or a pointer to where the value 1 will be stored if
  *	successful defragmentation occurs.
  *
- * Returns:
- *
- * \li	nmsg_res_success
- * \li	nmsg_res_again
+ * \return nmsg_res_success
+ * \return nmsg_res_again
  */
+nmsg_res
+nmsg_ipdg_parse_reasm(struct nmsg_ipdg *dg, unsigned etype, size_t len,
+		      const u_char *pkt, nmsg_ipreasm_t reasm,
+		      unsigned *new_len, u_char *new_pkt, int *defrag,
+		      uint64_t timestamp);
 
 #endif /* NMSG_IPDG_H */
