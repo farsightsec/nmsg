57b7faadcba6f7c154a7c68e8a686639057637a8
Robert Edmonds <edmonds@isc.org>
2009-07-23 09:57:12 -0400

    pbmodset: do module filename prefix check before module load

--- nmsg/pbmodset.c
+++ nmsg/pbmodset.c
@@ -126,17 +126,17 @@ nmsg_pbmodset_init(const char *path, int debug) {
 			/* not a module, skip */
 			continue;
 		}
-		dlmod = _nmsg_dlmod_init(fn);
-		if (dlmod == NULL) {
-			perror("_nmsg_dlmod_init");
-			free(pbmodset);
-			free(oldwd);
-			(void) closedir(dir);
-			return (NULL);
-		}
-		if (debug >= 4)
-			fprintf(stderr, "%s: trying %s\n", __func__, fn);
 		if (strstr(fn, NMSG_PBUF_MODULE_PREFIX "_") == fn) {
+			if (debug >= 4)
+				fprintf(stderr, "%s: trying %s\n", __func__, fn);
+			dlmod = _nmsg_dlmod_init(fn);
+			if (dlmod == NULL) {
+				perror("_nmsg_dlmod_init");
+				free(pbmodset);
+				free(oldwd);
+				(void) closedir(dir);
+				return (NULL);
+			}
 			if (debug >= 4)
 				fprintf(stderr, "%s: loading nmsgpb module %s\n",
 					__func__, fn);
