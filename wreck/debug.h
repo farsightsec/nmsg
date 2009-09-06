c3fab9d97fb7b2f2d292f628e4b735fad18fe11b
Robert Edmonds <edmonds@isc.org>
2009-09-06 12:42:42 -0400

    debug: use __FILE__ rather than __func__

--- wreck/debug.h
+++ wreck/debug.h
@@ -1,2 +1,2 @@
 #define DEBUG 1
-#define VERBOSE(format, ...) do { printf("%s(%d): " format, __func__, __LINE__, ## __VA_ARGS__); } while(0)
+#define VERBOSE(format, ...) do { printf("%s(%d): " format, __FILE__, __LINE__, ## __VA_ARGS__); } while (0)
