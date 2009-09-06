6653679d73fb56553d8776536ab0ef9c61b3cebc
Robert Edmonds <edmonds@isc.org>
2009-09-06 15:41:43 -0400

    parse_rdata: validate more types

--- wreck/msg/parse_rdata.c
+++ wreck/msg/parse_rdata.c
@@ -19,7 +19,7 @@ wreck_parse_rdata(const uint8_t *p, const uint8_t *eop, const uint8_t *ordata,
 		  size_t *alloc_bytes, uint8_t *dst)
 {
 	const uint8_t *rdata = ordata;
-	size_t len;
+	size_t len, bytes_read = 0;
 	uint8_t domain_name[255];
 	wreck_status status;
 
@@ -31,7 +31,7 @@ wreck_parse_rdata(const uint8_t *p, const uint8_t *eop, const uint8_t *ordata,
 				status = wreck_name_unpack(p, eop, rdata, domain_name, &len);
 				if (status != wreck_success)
 					WRECK_ERROR(wreck_err_parse_error);
-				wreck_name_skip(&rdata, eop);
+				bytes_read += wreck_name_skip(&rdata, eop);
 
 				if (alloc_bytes)
 					*alloc_bytes += len;
@@ -51,6 +51,39 @@ wreck_parse_rdata(const uint8_t *p, const uint8_t *eop, const uint8_t *ordata,
 			if (dst)
 				memcpy(dst, rdata, 20);
 			rdata += 20;
+			bytes_read += 20;
+
+			if (bytes_read != rdlen) {
+				VERBOSE("ERROR: IN/SOA rdlen=%u but read %zd bytes\n",
+					rdlen, bytes_read);
+				WRECK_ERROR(wreck_err_parse_error);
+			}
+			break;
+
+		case WRECK_DNS_TYPE_MX:
+			/* 16 bit integer: PREFERENCE */
+			if (alloc_bytes)
+				*alloc_bytes += 2;
+			if (dst)
+				memcpy(dst, rdata, 2);
+			rdata += 2;
+			bytes_read += 2;
+
+			/* EXCHANGE */
+			status = wreck_name_unpack(p, eop, rdata, domain_name, &len);
+			if (status != wreck_success)
+				WRECK_ERROR(wreck_err_parse_error);
+			bytes_read += wreck_name_skip(&rdata, eop);
+			if (bytes_read != rdlen) {
+				VERBOSE("ERROR: IN/MX rdlen=%u but read %zd bytes\n",
+					rdlen, bytes_read);
+				WRECK_ERROR(wreck_err_parse_error);
+			}
+
+			if (alloc_bytes)
+				*alloc_bytes += len;
+			if (dst)
+				memcpy(dst, domain_name, len);
 
 			break;
 
@@ -61,6 +94,9 @@ wreck_parse_rdata(const uint8_t *p, const uint8_t *eop, const uint8_t *ordata,
 				if (dst)
 					memcpy(dst, rdata, rdlen);
 				rdata += rdlen;
+			} else {
+				VERBOSE("ERROR: IN/A rdlen=%u, should be 4\n", rdlen);
+				WRECK_ERROR(wreck_err_parse_error);
 			}
 			break;
 
@@ -71,23 +107,69 @@ wreck_parse_rdata(const uint8_t *p, const uint8_t *eop, const uint8_t *ordata,
 				if (dst)
 					memcpy(dst, rdata, rdlen);
 				rdata += rdlen;
+			} else {
+				VERBOSE("ERROR: IN/AAAA rdlen=%u, should be 16\n", rdlen);
+				WRECK_ERROR(wreck_err_parse_error);
 			}
 			break;
 
 		case WRECK_DNS_TYPE_NS:
 		case WRECK_DNS_TYPE_CNAME:
 		case WRECK_DNS_TYPE_PTR:
-			status = wreck_name_unpack(p, eop, rdata, domain_name, &len);
-			if (status != wreck_success)
+		case WRECK_DNS_TYPE_MB:
+		case WRECK_DNS_TYPE_MD:
+		case WRECK_DNS_TYPE_MF:
+		case WRECK_DNS_TYPE_MG:
+		case WRECK_DNS_TYPE_MR:
+			/* these rdata types have a single domain name */
+			if (rdlen == 0) {
+				VERBOSE("ERROR: IN name rdata but no name\n");
 				WRECK_ERROR(wreck_err_parse_error);
+			}
+
+			status = wreck_name_unpack(p, eop, rdata, domain_name, &len);
+			if (status != wreck_success) {
+				VERBOSE("ERROR: IN name rdata contained invalid name\n");
+				WRECK_ERROR(wreck_err_parse_error);
+			}
 			if (alloc_bytes)
 				*alloc_bytes = len;
 			if (dst)
 				memcpy(dst, domain_name, len);
-			wreck_name_skip(&rdata, eop);
+
+			bytes_read = wreck_name_skip(&rdata, eop);
+			if (bytes_read != rdlen) {
+				VERBOSE("ERROR: IN name rdata rdlen=%u but read %zd bytes\n",
+					rdlen, bytes_read);
+				WRECK_ERROR(wreck_err_parse_error);
+			}
 			break;
 
-		case WRECK_DNS_TYPE_OPT:
+		case WRECK_DNS_TYPE_MINFO:
+		case WRECK_DNS_TYPE_RP:
+			/* these rdata types have two domain names */
+			/* technically RP should not be compressed (RFC 3597) */
+			for (int i = 0; i < 2; i++) {
+				status = wreck_name_unpack(p, eop, rdata, domain_name, &len);
+				if (status != wreck_success) {
+					VERBOSE("ERROR: IN 2name (#%d) rdata contained "
+						"invalid name\n", i);
+					WRECK_ERROR(wreck_err_parse_error);
+				}
+				if (alloc_bytes)
+					*alloc_bytes = len;
+				if (dst)
+					memcpy(dst, domain_name, len);
+
+				bytes_read += wreck_name_skip(&rdata, eop);
+			}
+			if (bytes_read != rdlen) {
+				VERBOSE("ERROR: IN 2name rdata rdlen=%u but read %zd bytes\n",
+					rdlen, bytes_read);
+				WRECK_ERROR(wreck_err_parse_error);
+			}
+			break;
+
 		case WRECK_DNS_TYPE_TXT:
 			if (alloc_bytes)
 				*alloc_bytes = rdlen;
