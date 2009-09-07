28d3043ca162388aab2a72c463bba89363cd0231
Robert Edmonds <edmonds@isc.org>
2009-09-07 14:00:26 -0400

    initial autoconfiscation

--- wreck/msg/private.h
+++ wreck/msg/private.h
@@ -6,3 +6,9 @@
 #include <string.h>
 
 #include <wreck.h>
+
+#if DEBUG
+# define VERBOSE(format, ...) do { printf("%s(%d): " format, __FILE__, __LINE__, ## __VA_ARGS__); } while (0)
+#else
+# define VERBOSE(format, ...)
+#endif
