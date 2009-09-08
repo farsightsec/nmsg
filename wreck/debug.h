4e43bc134c84d36703adc4b0ff299ef17347a09d
Robert Edmonds <edmonds@isc.org>
2009-09-07 20:33:48 -0400

    remove unused header files

--- wreck/debug.h
+++
@@ -1,2 +0,0 @@
-#define DEBUG 1
-#define VERBOSE(format, ...) do { printf("%s(%d): " format, __FILE__, __LINE__, ## __VA_ARGS__); } while (0)
--- wreck/nodebug.h
+++
@@ -1,2 +0,0 @@
-#define DEBUG 0
-#define VERBOSE(format, ...)
