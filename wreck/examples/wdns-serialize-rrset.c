048d690f3e20fd8be84cacdce315a62fc1bbc97a
Robert Edmonds <edmonds@isc.org>
2009-09-26 15:12:38 -0400

    examples/wdns-serialize-rrset: wdns_rrset_array_t reduction

--- wreck/examples/wdns-serialize-rrset.c
+++ wreck/examples/wdns-serialize-rrset.c
@@ -34,7 +34,7 @@ serialize_rrsets(wdns_message_t *m)
 	for (size_t sec = WDNS_MSG_SEC_ANSWER; sec < WDNS_MSG_SEC_MAX; sec++) {
 		a = &m->sections[sec];
 		for (size_t n = 0; n < a->n_rrsets; n++) {
-			rrset = a->rrsets[n];
+			rrset = &a->rrsets[n];
 			wdns_serialize_rrset(rrset, NULL, &sz);
 			buf = alloca(sz);
 			wdns_serialize_rrset(rrset, buf, NULL);
