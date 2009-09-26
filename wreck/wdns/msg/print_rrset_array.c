bd42e54553224ce7f9d0ad88d4d1a235e4b081b1
Robert Edmonds <edmonds@isc.org>
2009-09-26 15:12:26 -0400

    print_rrset_array: wdns_rrset_array_t reduction

--- wreck/wdns/msg/print_rrset_array.c
+++ wreck/wdns/msg/print_rrset_array.c
@@ -4,5 +4,5 @@ void
 wdns_print_rrset_array(FILE *fp, wdns_rrset_array_t *a, unsigned sec)
 {
 	for (unsigned i = 0; i < a->n_rrsets; i++)
-		wdns_print_rrset(fp, a->rrsets[i], sec);
+		wdns_print_rrset(fp, &a->rrsets[i], sec);
 }
