3230a28604376540cecf48c4eb4f77194d0902c5
Robert Edmonds <edmonds@isc.org>
2009-11-12 15:32:23 -0500

    nmsg/msgmod: calculate mod->n_fields at load time

--- nmsg/msgmod/transparent.c
+++ nmsg/msgmod/transparent.c
@@ -40,7 +40,7 @@ _nmsg_msgmod_lookup_field(struct nmsg_msgmod *mod, const char *name) {
 
 	res = bsearch(&key,
 		      &mod->fields[0],
-		      mod->plugin->pbdescr->n_fields,
+		      mod->n_fields,
 		      sizeof(struct nmsg_msgmod_field),
 		      _nmsg_msgmod_field_cmp);
 
@@ -71,9 +71,18 @@ _nmsg_msgmod_load_field_descriptors(struct nmsg_msgmod *mod) {
 		if (descr_found == false)
 			return (nmsg_res_failure);
 	}
+
+	/* count number of plugin fields */
+	mod->n_fields = 0;
+	for (field = &mod->plugin->fields[0];
+	     field->name != NULL;
+	     field++)
+	{
+		mod->n_fields += 1;
+	}
+
 	/* create nmsg_msgmod_field table from plugin's fields */
-	mod->fields = calloc(1, sizeof(struct nmsg_msgmod_field) *
-			     (mod->plugin->pbdescr->n_fields + 1));
+	mod->fields = calloc(1, sizeof(struct nmsg_msgmod_field) * (mod->n_fields + 1));
 	if (mod->fields == NULL)
 		return (nmsg_res_memfail);
 
@@ -89,7 +98,7 @@ _nmsg_msgmod_load_field_descriptors(struct nmsg_msgmod *mod) {
 
 	/* sort field descriptors by name */
 	qsort(&mod->fields[0],
-	      mod->plugin->pbdescr->n_fields,
+	      mod->n_fields,
 	      sizeof(struct nmsg_msgmod_field),
 	      _nmsg_msgmod_field_cmp);
 
