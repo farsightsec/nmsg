e4c695051d4f8e09009c32c349f789c9cf7e52fb
Robert Edmonds <edmonds@isc.org>
2009-11-16 20:03:31 -0500

    nmsg/pynmsg: add output.open_callback()

--- pynmsg/nmsg_output.pyx
+++ pynmsg/nmsg_output.pyx
@@ -14,12 +14,29 @@ def output_open_sock(addr, port, size_t bufsz=NMSG_WBUFSZ_ETHER):
     o.fileobj = obj
     return o
 
+def output_open_callback(func):
+    o = output()
+    o._open_callback(func)
+    return o
+
+cdef void callback(nmsg_message_t _msg, void *user) with gil:
+    cdef _recv_message msg
+
+    msg = _recv_message()
+    msg.set_instance(_msg)
+
+    try:
+        (<object>user)(msg)
+    finally:
+        msg._instance = NULL
+
 cdef class output(object):
     cdef nmsg_output_t _instance
     cdef public object fileobj
 
     open_file = staticmethod(output_open_file)
     open_sock = staticmethod(output_open_sock)
+    open_callback = staticmethod(output_open_callback)
 
     def __cinit__(self):
         self._instance = NULL
@@ -37,3 +54,8 @@ cdef class output(object):
         self._instance = nmsg_output_open_sock(fileno, bufsz)
         if self._instance == NULL:
             raise Exception, 'nmsg_output_open_sock() failed'
+
+    cpdef _open_callback(self, object func):
+        self._instance = nmsg_output_open_callback(<nmsg_cb_message>callback, <void*>func)
+        if self._instance == NULL:
+            raise Exception, 'nmsg_output_open_callback() failed'
