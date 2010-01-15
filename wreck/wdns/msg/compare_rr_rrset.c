ac29816ee8c3656bc0060ad2739893d3ad3a2c4e
Robert Edmonds <edmonds@isc.org>
2010-01-15 17:01:54 -0500

    [wreck] compare_rr_rrset: perform case-insensitive name comparison

--- wreck/wdns/msg/compare_rr_rrset.c
+++ wreck/wdns/msg/compare_rr_rrset.c
@@ -3,10 +3,8 @@
 /**
  * Compare an RR to a RRset. An RR and an RRset compare true if the name, type,
  * and class match.
-
- * This function does not do a case-insensitive name comparison. The caller
- * should ensure that the owner names being compared have been downcased with
- * wdns_downcase_name().
+ *
+ * This function does a case-insensitive name comparison.
  *
  * \param[in] rr the RR to compare
  * \param[in] rrset the RRset to compare
@@ -21,7 +19,20 @@ wdns_compare_rr_rrset(const wdns_rr_t *rr, const wdns_rrset_t *rrset)
 	    rr->rrtype == rrset->rrtype &&
 	    rr->rrclass == rrset->rrclass)
 	{
-		return (memcmp(rr->name.data, rrset->name.data, rr->name.len) == 0);
+		wdns_name_t name_rr;
+		wdns_name_t name_rrset;
+
+		name_rr.len = rr->name.len;
+		name_rr.data = alloca(name_rr.len);
+		memcpy(name_rr.data, rr->name.data, name_rr.len);
+		wdns_downcase_name(&name_rr);
+
+		name_rrset.len = rrset->name.len;
+		name_rrset.data = alloca(name_rrset.len);
+		memcpy(name_rrset.data, rrset->name.data, name_rrset.len);
+		wdns_downcase_name(&name_rrset);
+
+		return (memcmp(name_rr.data, name_rrset.data, name_rr.len) == 0);
 	}
 
 	return (false);
