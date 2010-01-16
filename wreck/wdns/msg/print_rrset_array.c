30b1ebcdd3b9b27b48da6bbe5f2176d8bb6b80a8
Robert Edmonds <edmonds@isc.org>
2010-01-16 00:24:24 -0500

    [wreck] remove print_message, print_rr, print_rrset, print_rrset_array

--- wreck/wdns/msg/print_rrset_array.c
+++
@@ -1,8 +0,0 @@
-#include "private.h"
-
-void
-wdns_print_rrset_array(FILE *fp, wdns_rrset_array_t *a, unsigned sec)
-{
-	for (unsigned i = 0; i < a->n_rrs; i++)
-		wdns_print_rr(fp, &a->rrs[i], sec);
-}
