f3466a5742ced93a831d4de69e85932f8b63dc98
Robert Edmonds <edmonds@isc.org>
2010-01-15 19:41:22 -0500

    [wreck] print_rrset_array: use wdns_print_rr() instead of wdns_print_rrset()

--- wreck/wdns/msg/print_rrset_array.c
+++ wreck/wdns/msg/print_rrset_array.c
@@ -3,6 +3,6 @@
 void
 wdns_print_rrset_array(FILE *fp, wdns_rrset_array_t *a, unsigned sec)
 {
-	for (unsigned i = 0; i < a->n_rrsets; i++)
-		wdns_print_rrset(fp, &a->rrsets[i], sec);
+	for (unsigned i = 0; i < a->n_rrs; i++)
+		wdns_print_rr(fp, &a->rrs[i], sec);
 }
