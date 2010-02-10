ce94723b7be9deab8a93e39958f9abc10031c106
Robert Edmonds <edmonds@isc.org>
2010-02-10 18:57:05 -0500

    pynmsg: add ../protobuf-c to include dirs

--- pynmsg/setup.py
+++ pynmsg/setup.py
@@ -8,7 +8,7 @@ setup(
         Extension('nmsg', ['nmsg.pyx'],
             libraries = ['nmsg'],
             library_dirs = ['../nmsg/.libs'],
-            include_dirs = ['../nmsg', '..']
+            include_dirs = ['../nmsg', '../protobuf-c', '..']
         )
     ],
     cmdclass = {'build_ext': build_ext},
