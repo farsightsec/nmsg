d98a05f246d7c8134e58458b41dd20a42132c4fb
Robert Edmonds <edmonds@isc.org>
2010-02-26 19:44:26 -0500

    pynmsg: rename nmsg.so to _nmsg.so

--- pynmsg/setup.py
+++ pynmsg/setup.py
@@ -3,9 +3,9 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 setup(
-    name = 'nmsg',
+    name = '_nmsg',
     ext_modules = [
-        Extension('nmsg', ['nmsg.pyx'],
+        Extension('_nmsg', ['nmsg.pyx'],
             libraries = ['nmsg'],
             library_dirs = ['../nmsg/.libs'],
             include_dirs = ['../nmsg', '../protobuf-c', '..']
