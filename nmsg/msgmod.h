96eaa74cb2b498fff1fade20e908af518a04e310
Robert Edmonds <edmonds@isc.org>
2010-03-31 18:26:40 -0400

    msgmod: doc update

--- nmsg/msgmod.h
+++ nmsg/msgmod.h
@@ -23,7 +23,7 @@
  * Message modules extend nmsg by allowing new message types to be implemented
  * in dynamically loaded plugins. Msgmods identify the types of messages they
  * can handle by registering a vendor ID number and a per-vendor message type
- * number with the msgmod loader. Functions for creating and interpreting nmsg
+ * number with the msgmod loader. Functions for creating and interpreting NMSG
  * payloads must be provided.
  *
  * Msgmods are dynamically loaded shared objects that must provide either a
@@ -52,14 +52,13 @@
  * the nmsg distribution. The file <tt>nmsg/isc/email.proto</tt> is compiled
  * with the <a href="http://code.google.com/p/protobuf-c/">Protobuf-C
  * compiler</a> into the files email.pb-c.c and email.pb-c.h. The file
- * nmsgpb_isc_email.h provides the message type number assignment and
- * nmsgpb_isc_email.c provides the C stub to interface with the msgmod.h
+ * email.c provides the C stub to interface with the msgmod
  * interface, which is compiled into a shared object and installed into the
  * nmsg module directory.
  *
  * <b>MP:</b>
- *	\li nmsg_msgmod_init() returns an opaque pointer which must be used to
- *	differentiate threads.
+ *	\li nmsg_msgmod_init() returns an opaque pointer which is used to
+ *	differentiate threads if a message module is not stateless.
  */
 
 #include <sys/types.h>
@@ -124,15 +123,15 @@ typedef enum {
 
 #define NMSG_MSGMOD_FIELD_REPEATED      0x01
 #define NMSG_MSGMOD_FIELD_REQUIRED      0x02
-#define NMSG_MSGMOD_FIELD_HIDDEN        0x04
-#define NMSG_MSGMOD_FIELD_NOPRINT       0x08
+#define NMSG_MSGMOD_FIELD_HIDDEN        0x04	/*%< hide field from the message API */
+#define NMSG_MSGMOD_FIELD_NOPRINT       0x08	/*%< don't print the field */
 
 /**
  * Initialize a message module.
  *
- * \param[in] mod initialized msgmod.
+ * \param[in] mod Initialized msgmod.
  *
- * \param[out] clos opaque pointer specific to this instantiation of the module.
+ * \param[out] clos Opaque pointer specific to this instantiation of the module.
  *	This pointer must be supplied to nmsg_msgmod functions taking a 'clos'
  *	parameter.
  *
@@ -147,9 +146,9 @@ nmsg_msgmod_init(nmsg_msgmod_t mod, void **clos);
 /**
  * Finalize a mesage module.
  *
- * \param[in] mod initialized msgmod.
+ * \param[in] mod Initialized msgmod.
  *
- * \param[in] clos opaque pointer returned by the module initialization
+ * \param[in] clos Opaque pointer returned by the module initialization
  *	function.
  *
  * \return #nmsg_res_success
@@ -160,7 +159,7 @@ nmsg_res
 nmsg_msgmod_fini(nmsg_msgmod_t mod, void **clos);
 
 /**
- * Convert a presentation format line to an nmsg payload.
+ * Convert a presentation format line to an NMSG payload.
  * Since the presentation format stream is line-delimited, not every line
  * will necessarily result in a serialized message.
  *
@@ -170,12 +169,12 @@ nmsg_msgmod_fini(nmsg_msgmod_t mod, void **clos);
  * Msgmods are not required to implement a function to convert presentation form
  * data to payloads, in which case #nmsg_res_notimpl will be returned.
  *
- * \param[in] mod initialized msgmod.
+ * \param[in] mod Initialized msgmod.
  *
- * \param[in] clos opaque pointer returned by the module initialization
+ * \param[in] clos Opaque pointer returned by the module initialization
  *	function.
  *
- * \param[in] pres line of presentation form input of the type handled by 'mod'.
+ * \param[in] pres Line of presentation form input of the type handled by 'mod'.
  *
  * \return #nmsg_res_success
  * \return #nmsg_res_failure
@@ -192,14 +191,14 @@ nmsg_msgmod_pres_to_payload(nmsg_msgmod_t mod, void *clos, const char *pres);
  * function will return the serialized payload. The caller is responsible for
  * freeing the payload returned.
  *
- * \param[in] mod initialized msgmod.
+ * \param[in] mod Initialized msgmod.
  *
- * \param[in] clos opaque pointer returned by the module initialization
+ * \param[in] clos Opaque pointer returned by the module initialization
  *	function.
  *
- * \param[out] pbuf serialized payload.
+ * \param[out] pbuf Serialized payload.
  *
- * \param[out] sz length of the serialized payload.
+ * \param[out] sz Length of the serialized payload.
  *
  * \return #nmsg_res_success
  * \return #nmsg_res_failure
@@ -211,21 +210,21 @@ nmsg_msgmod_pres_to_payload_finalize(nmsg_msgmod_t mod, void *clos, uint8_t **pb
 				     size_t *sz);
 
 /**
- * Convert an IP datagram to an nmsg payload.
+ * Convert an IP datagram to an NMSG payload.
  *
  * Msgmods are not required to implement a function to convert IP datagrams to
  * payloads, in which case #nmsg_res_notimpl will be returned.
  *
- * \param[in] mod initialized msgmod.
+ * \param[in] mod Initialized msgmod.
  *
- * \param[in] clos opaque pointer returned by the module initialization
+ * \param[in] clos Opaque pointer returned by the module initialization
  *	function.
  *
- * \param[in] dg filled nmsg_ipdg structure.
+ * \param[in] dg Filled nmsg_ipdg structure.
  *
- * \param[out] pbuf serialized payload.
+ * \param[out] pbuf Serialized payload.
  *
- * \param[out] sz length of the serialized payload.
+ * \param[out] sz Length of the serialized payload.
  *
  * \return #nmsg_res_parse_error
  * \return #nmsg_res_pbuf_ready
@@ -240,9 +239,9 @@ nmsg_msgmod_ipdg_to_payload(nmsg_msgmod_t mod, void *clos,
  * Determine which nmsg_msgmod is responsible for a given vid/msgtype tuple,
  * if any.
  *
- * \param[in] vid numeric vendor ID.
+ * \param[in] vid Numeric vendor ID.
  *
- * \param[in] msgtype numeric message type.
+ * \param[in] msgtype Numeric message type.
  *
  * \return The nmsg_msgmod responsible for handling the given vid/msgtype tuple,
  *	if such a module has been loaded into the set, or NULL otherwise.
@@ -254,9 +253,9 @@ nmsg_msgmod_lookup(unsigned vid, unsigned msgtype);
  * Determine which nmsg_msgmod is responsible for a given vid/msgtype tuple,
  * if any. This function looks up the vid and msgtype by name.
  *
- * \param[in] vname vendor name.
+ * \param[in] vname Vendor name.
  *
- * \param[in] mname message type name.
+ * \param[in] mname Message type name.
  *
  * \return The nmsg_msgmod responsible for handling the given vid/msgtype tuple,
  *	if such a module has been loaded into the set, or NULL otherwise.
@@ -267,9 +266,9 @@ nmsg_msgmod_lookup_byname(const char *vname, const char *mname);
 /**
  * Convert the human-readable name of a message type to a message type ID.
  *
- * \param[in] vid numeric vendor ID.
+ * \param[in] vid Numeric vendor ID.
  *
- * \param[in] mname message type name.
+ * \param[in] mname Message type name.
  *
  * \return A numeric message type ID. By convention, 0 is used to indicate an
  *	unknown message type.
@@ -281,9 +280,9 @@ nmsg_msgmod_mname_to_msgtype(unsigned vid, const char *mname);
  * Convert a vendor ID / message type ID tuple to the human-readable form
  * of the message type.
  *
- * \param[in] vid numeric vendor ID.
+ * \param[in] vid Numeric vendor ID.
  *
- * \param[in] msgtype numeric message type.
+ * \param[in] msgtype Numeric message type.
  *
  * \return A human-readable message type name. NULL is returned if the vendor ID
  *	or message type is unknown.
@@ -294,7 +293,7 @@ nmsg_msgmod_msgtype_to_mname(unsigned vid, unsigned msgtype);
 /**
  * Convert a numeric vendor ID to its human-readable name.
  *
- * \param[in] vid numeric vendor ID.
+ * \param[in] vid Numeric vendor ID.
  *
  * \return A human-readable vendor name. NULL is returned if the vendor ID is
  *	unknown.
@@ -305,7 +304,7 @@ nmsg_msgmod_vid_to_vname(unsigned vid);
 /**
  * Convert a human-readable vendor name to its numeric ID.
  *
- * \param[in] vname vendor name.
+ * \param[in] vname Vendor name.
  *
  * \return A numeric vendor ID. By convention, 0 is used to indicate an unknown
  *	vendor ID.
@@ -315,8 +314,6 @@ nmsg_msgmod_vname_to_vid(const char *vname);
 
 /**
  * Return the maximum vendor ID.
- *
- * \return maximum vendor ID.
  */
 unsigned
 nmsg_msgmod_get_max_vid(void);
@@ -324,9 +321,7 @@ nmsg_msgmod_get_max_vid(void);
 /**
  * Return the maximum message type registered to a vendor ID.
  *
- * \param[in] vid numeric vendor ID.
- *
- * \return maximum message type.
+ * \param[in] vid Numeric vendor ID.
  */
 unsigned
 nmsg_msgmod_get_max_msgtype(unsigned vid);
