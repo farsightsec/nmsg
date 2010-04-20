583bcb2e20a54da834cd989c5605b5c1db9a2094
Robert Edmonds <edmonds@isc.org>
2010-04-19 21:51:46 -0400

    nmsg/isc/dnsqr: add query hash table and response matching

--- nmsg/isc/dnsqr.c
+++ nmsg/isc/dnsqr.c
@@ -18,18 +18,49 @@
 
 /* Import. */
 
+#include <sys/mman.h>
+#include <pthread.h>
+
 #include <wdns.h>
+
+#include "lookup3.h"
 
 #include "dnsqr.pb-c.c"
 
+/* Macros. */
+
+#define NUM_SLOTS	32768
+#define MAX_VALUES	20480
+
 /* Exported via module context. */
 
-static nmsg_res
-dnsqr_pkt_to_payload(void *clos, nmsg_pcap_t pcap, nmsg_message_t *m);
+//nmsg_res dnsqr_init(void **clos);
+//nmsg_res dnsqr_fini(void **clos);
+
+//nmsg_res dnsqr_pkt_to_payload(void *clos, nmsg_pcap_t pcap, nmsg_message_t *m);
 
 /* Data structures. */
 
-#if 0
+typedef struct {
+	Nmsg__Isc__DnsQR	*dnsqr;
+	uint32_t		fifo_slot;
+} hash_entry_t;
+
+typedef struct {
+	pthread_mutex_t		lock;
+
+	hash_entry_t		*entries;
+	uint32_t		*fifo;
+
+	size_t			len_entries;
+	size_t			len_fifo;
+
+	uint32_t		num_slots;
+	uint32_t		max_values;
+	uint32_t		fifo_idx;
+	uint32_t		count;
+} dnsqr_ctx_t;
+
 typedef struct {
 	uint32_t	query_ip;
 	uint32_t	response_ip;
@@ -39,7 +70,6 @@ typedef struct {
 	uint16_t	id;
 	uint16_t	qtype;
 	uint16_t	qclass;
-	uint8_t		qname[];
 } dnsqr_key_t;
 
 typedef struct {
@@ -51,9 +81,7 @@ typedef struct {
 	uint16_t	id;
 	uint16_t	qtype;
 	uint16_t	qclass;
-	uint8_t		qname[];
 } dnsqr_key6_t;
-#endif
 
 /* Data. */
 
@@ -114,19 +142,269 @@ struct nmsg_msgmod_field dnsqr_fields[] = {
 	NMSG_MSGMOD_FIELD_END
 };
 
-/* Export. */
+nmsg_res
+dnsqr_init(void **clos) {
+	dnsqr_ctx_t *ctx;
 
-struct nmsg_msgmod_plugin nmsg_msgmod_ctx = {
-	.msgver = NMSG_MSGMOD_VERSION,
-	.vendor = NMSG_VENDOR_ISC,
-	.msgtype = { NMSG_VENDOR_ISC_DNSQR_ID, NMSG_VENDOR_ISC_DNSQR_NAME },
+	ctx = calloc(1, sizeof(*ctx));
+	if (ctx == NULL)
+		return (nmsg_res_memfail);
 
-	.pbdescr = &nmsg__isc__dns_qr__descriptor,
-	.fields = dnsqr_fields,
-	.pkt_to_payload = dnsqr_pkt_to_payload,
-};
+	pthread_mutex_init(&ctx->lock, NULL);
 
-static nmsg_message_t
+	ctx->num_slots = NUM_SLOTS;
+	ctx->max_values = MAX_VALUES;
+
+	ctx->len_entries = sizeof(hash_entry_t) * ctx->num_slots;
+	ctx->len_fifo = sizeof(uint32_t) * ctx->max_values;
+
+	ctx->entries = mmap(NULL, ctx->len_entries,
+			    PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+	if (ctx->entries == MAP_FAILED) {
+		free(ctx);
+		return (nmsg_res_memfail);
+	}
+
+	ctx->fifo = mmap(NULL, ctx->len_fifo,
+			 PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+	if (ctx->fifo == MAP_FAILED) {
+		munmap(ctx->entries, ctx->len_entries);
+		free(ctx);
+		return (nmsg_res_memfail);
+	}
+
+	*clos = ctx;
+	//fprintf(stderr, "%s: clos=%p ctx=%p\n", __func__, clos, ctx);
+
+	return (nmsg_res_success);
+}
+
+nmsg_res
+dnsqr_fini(void **clos) {
+	size_t n;
+	dnsqr_ctx_t *ctx;
+
+	ctx = (dnsqr_ctx_t *) *clos;
+
+	for (n = 0; n < ctx->num_slots; n++) {
+		hash_entry_t *he = &ctx->entries[n];
+		if (he->dnsqr != NULL)
+			nmsg__isc__dns_qr__free_unpacked(he->dnsqr, NULL);
+	}
+
+	munmap(ctx->entries, ctx->len_entries);
+	munmap(ctx->fifo, ctx->len_fifo);
+	free(ctx);
+	*clos = NULL;
+
+	return (nmsg_res_success);
+}
+
+bool
+dnsqr_eq(Nmsg__Isc__DnsQR *d1, Nmsg__Isc__DnsQR *d2) {
+	if (d1->id == d2->id &&
+	    d1->query_port == d2->query_port &&
+	    d1->response_port == d2->response_port &&
+	    d1->qname.len == d2->qname.len &&
+	    d1->qtype == d2->qtype &&
+	    d1->qclass == d2->qclass &&
+	    d1->ip_proto == d2->ip_proto &&
+	    d1->query_ip.len == d2->query_ip.len &&
+	    d1->response_ip.len == d2->response_ip.len)
+	{
+		if (memcmp(d1->query_ip.data, d2->query_ip.data, d1->query_ip.len) == 0 &&
+		    memcmp(d1->response_ip.data, d2->response_ip.data, d1->response_ip.len) == 0 &&
+		    memcmp(d1->qname.data, d2->qname.data, d1->qname.len) == 0)
+		{
+			return (true);
+		}
+	}
+
+	return (false);
+}
+
+uint32_t
+dnsqr_hash(Nmsg__Isc__DnsQR *dnsqr) {
+	dnsqr_key_t key;
+	dnsqr_key6_t key6;
+	size_t len;
+	void *k;
+	uint32_t hash;
+
+	assert(dnsqr->query_ip.len == 4 || dnsqr->query_ip.len == 16);
+	assert(dnsqr->response_ip.len == 4 || dnsqr->response_ip.len == 16);
+
+	if (dnsqr->query_ip.len == 4) {
+		memcpy(&key.query_ip, dnsqr->query_ip.data, 4);
+		memcpy(&key.response_ip, dnsqr->response_ip.data, 4);
+		key.ip_proto = dnsqr->ip_proto;
+		key.query_port = dnsqr->query_port;
+		key.response_port = dnsqr->response_port;
+		key.id = dnsqr->id;
+		key.qtype = dnsqr->qtype;
+		key.qclass = dnsqr->qclass;
+		k = &key;
+		len = sizeof(key);
+	} else if (dnsqr->query_ip.len == 16) {
+		memcpy(&key6.query_ip6, dnsqr->query_ip.data, 16);
+		memcpy(&key6.response_ip6, dnsqr->response_ip.data, 16);
+		key6.ip_proto = dnsqr->ip_proto;
+		key6.query_port = dnsqr->query_port;
+		key6.response_port = dnsqr->response_port;
+		key6.id = dnsqr->id;
+		key6.qtype = dnsqr->qtype;
+		key6.qclass = dnsqr->qclass;
+		k = &key6;
+		len = sizeof(key6);
+	} else {
+		assert(0);
+	}
+
+	hash = hashlittle(k, len, 0);
+	hash = hashlittle(dnsqr->qname.data, dnsqr->qname.len, hash);
+	return (hash);
+}
+
+uint32_t
+dnsqr_insert_fifo(dnsqr_ctx_t *ctx, uint32_t val) {
+	uint32_t ret_idx = ctx->fifo_idx;
+
+	ctx->fifo[ctx->fifo_idx] = val;
+
+	ctx->fifo_idx += 1;
+	if (ctx->fifo_idx >= ctx->max_values)
+		ctx->fifo_idx = 0;
+
+	return (ret_idx);
+}
+
+void
+dnsqr_insert_query(dnsqr_ctx_t *ctx, Nmsg__Isc__DnsQR *dnsqr) {
+	bool miss = false;
+	uint32_t hash;
+	unsigned slot, slot_stop;
+
+	hash = dnsqr_hash(dnsqr);
+	slot = hash % ctx->num_slots;
+
+	if (slot > 0)
+		slot_stop = slot - 1;
+	else
+		slot_stop = ctx->num_slots - 1;
+
+	/* lock hash table */
+	pthread_mutex_lock(&ctx->lock);
+
+	for (;;) {
+		hash_entry_t *he = &ctx->entries[slot];
+
+		/* empty slot, insert entry */
+		if (he->dnsqr == NULL) {
+			ctx->count += 1;
+			he->dnsqr = dnsqr;
+			he->fifo_slot = dnsqr_insert_fifo(ctx, slot);
+			miss = true;
+			break;
+		}
+
+		/* slot filled */
+		assert(slot != slot_stop);
+		slot += 1;
+		if (slot >= ctx->num_slots)
+			slot = 0;
+	}
+
+	/* unlock hash table */
+	pthread_mutex_unlock(&ctx->lock);
+}
+
+void
+dnsqr_remove_slot(dnsqr_ctx_t *ctx, unsigned slot) {
+	hash_entry_t *he;
+	unsigned i, j, k;
+
+	i = j = slot;
+	he = &ctx->entries[slot];
+
+	assert(he->dnsqr != NULL);
+	he->dnsqr = NULL;
+	he->fifo_slot = 0;
+
+	for (;;) {
+		/* j is the current slot of the next value */
+		j = (j + 1) % ctx->num_slots;
+		if (ctx->entries[j].dnsqr == NULL) {
+			/* slot is unoccupied */
+			break;
+		}
+
+		/* k is the natural slot of the value at slot j */
+		k = dnsqr_hash(ctx->entries[j].dnsqr) % ctx->num_slots;
+		if ((j > i && (k <= i || k > j)) ||
+		    (j < i && (k <= i && k > j)))
+		{
+			/* this value needs to be moved up,
+			 * as k is cyclically between i and j */
+			memcpy(&ctx->entries[i], &ctx->entries[j], sizeof(hash_entry_t));
+
+			/* fix up the fifo slot index to point to the new slot */
+			ctx->fifo[ctx->entries[i].fifo_slot] = i;
+
+			/* delete the value at the old slot */
+			memset(&ctx->entries[j], 0, sizeof(hash_entry_t));
+
+			/* check the next slot */
+			i = j;
+		}
+	}
+}
+
+Nmsg__Isc__DnsQR *
+dnsqr_retrieve(dnsqr_ctx_t *ctx, Nmsg__Isc__DnsQR *dnsqr) {
+	Nmsg__Isc__DnsQR *query;
+	uint32_t hash;
+	unsigned slot, slot_stop;
+
+	hash = dnsqr_hash(dnsqr);
+	slot = hash % ctx->num_slots;
+
+	if (slot > 0)
+		slot_stop = slot - 1;
+	else
+		slot_stop = ctx->num_slots - 1;
+
+	/* lock hash table */
+	pthread_mutex_lock(&ctx->lock);
+
+	for (;;) {
+		hash_entry_t *he = &ctx->entries[slot];
+
+		/* empty slot, return failure */
+		if (he->dnsqr == NULL) {
+			pthread_mutex_unlock(&ctx->lock);
+			return (NULL);
+		}
+
+		/* slot filled, compare */
+		if (dnsqr_eq(dnsqr, he->dnsqr) == true) {
+			query = he->dnsqr;
+			dnsqr_remove_slot(ctx, slot);
+			pthread_mutex_unlock(&ctx->lock);
+			return (query);
+		}
+
+		/* slot filled, but not our slot */
+		assert(slot != slot_stop);
+		slot += 1;
+		if (slot >= ctx->num_slots)
+			slot = 0;
+	}
+
+	/* unlock hash table */
+	pthread_mutex_unlock(&ctx->lock);
+}
+
+nmsg_message_t
 dnsqr_to_message(Nmsg__Isc__DnsQR *dnsqr) {
 	ProtobufCBufferSimple sbuf;
 	size_t buf_sz;
@@ -149,8 +427,8 @@ dnsqr_to_message(Nmsg__Isc__DnsQR *dnsqr) {
 					      sbuf.data, buf_sz, NULL));
 }
 
-static nmsg_res
-do_packet_dns(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, int *qr) {
+nmsg_res
+do_packet_dns(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, bool *qr) {
 	const uint8_t *p;
 	size_t len;
 	size_t t;
@@ -204,8 +482,8 @@ do_packet_dns(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, int *qr) {
 	return (nmsg_res_success);
 }
 
-static nmsg_res
-do_packet_udp(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, int *qr) {
+nmsg_res
+do_packet_udp(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, bool *qr) {
 	const struct udphdr *udp;
 	nmsg_res res;
 	uint16_t src_port;
@@ -222,7 +500,7 @@ do_packet_udp(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, int *qr) {
 	if (res != nmsg_res_success)
 		return (res);
 
-	if (*qr == 0) {
+	if (*qr == false) {
 		/* message is a query */
 		dnsqr->query_port = src_port;
 		dnsqr->response_port = dst_port;
@@ -235,8 +513,8 @@ do_packet_udp(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, int *qr) {
 	return (nmsg_res_success);
 }
 
-static nmsg_res
-do_packet_v4(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, int *qr) {
+nmsg_res
+do_packet_v4(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, bool *qr) {
 	const struct ip *ip;
 	nmsg_res res;
 	uint32_t ip4;
@@ -269,7 +547,7 @@ do_packet_v4(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, int *qr) {
 
 	dnsqr->ip_proto = dg->proto_transport;
 
-	if (*qr == 0) {
+	if (*qr == false) {
 		/* message is a query */
 		memcpy(dnsqr->query_ip.data, &ip->ip_src, 4);
 		memcpy(dnsqr->response_ip.data, &ip->ip_dst, 4);
@@ -282,8 +560,8 @@ do_packet_v4(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, int *qr) {
 	return (nmsg_res_success);
 }
 
-static nmsg_res
-do_packet_v6(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, int *qr) {
+nmsg_res
+do_packet_v6(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, bool *qr) {
 	const struct ip6_hdr *ip6;
 	nmsg_res res;
 
@@ -315,7 +593,7 @@ do_packet_v6(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, int *qr) {
 
 	dnsqr->ip_proto = dg->proto_transport;
 
-	if (*qr == 0) {
+	if (*qr == false) {
 		/* message is a query */
 		memcpy(dnsqr->query_ip.data, &ip6->ip6_src, 16);
 		memcpy(dnsqr->response_ip.data, &ip6->ip6_dst, 16);
@@ -328,18 +606,23 @@ do_packet_v6(Nmsg__Isc__DnsQR *dnsqr, struct nmsg_ipdg *dg, int *qr) {
 	return (nmsg_res_success);
 }
 
-static nmsg_res
-do_packet(nmsg_pcap_t pcap, nmsg_message_t *m,
+void
+dnsqr_merge(Nmsg__Isc__DnsQR *d1, Nmsg__Isc__DnsQR *d2) {
+	nmsg__isc__dns_qr__free_unpacked(d1, NULL);
+}
+
+nmsg_res
+do_packet(dnsqr_ctx_t *ctx, nmsg_pcap_t pcap, nmsg_message_t *m,
 	  const uint8_t *pkt, struct pcap_pkthdr *pkt_hdr)
 {
 	Nmsg__Isc__DnsQR *dnsqr = NULL;
-	int qr = 0;
+	bool qr = 0;
 	nmsg_res res;
 	struct nmsg_ipdg dg;
 
 	res = nmsg_ipdg_parse_pcap_raw(&dg, pcap, pkt_hdr, pkt);
 	if (res != nmsg_res_success)
-		goto out;
+		return (res);
 
 	/* XXX if it's a fragment, do something else here */
 
@@ -365,23 +648,36 @@ do_packet(nmsg_pcap_t pcap, nmsg_message_t *m,
 
 	if (qr == 0) {
 		/* message is a query */
-
-		/* XXX insert into hash table and return */
 		dnsqr->type = NMSG__ISC__DNS_QRTYPE__UDP_UNANSWERED_QUERY;
+		dnsqr_insert_query(ctx, dnsqr);
+		dnsqr = NULL;
+		res = nmsg_res_again;
 	} else {
-		/* message is a response*/
+		/* message is a response */
+		Nmsg__Isc__DnsQR *query;
 
-		/* XXX
-		 * look up in hash table
-		 * if found, merge query and response, generate message, return
-		 * else set type to unsolicited response, generate message, return
-		 */
-		dnsqr->type = NMSG__ISC__DNS_QRTYPE__UDP_UNSOLICITED_RESPONSE;
+		query = dnsqr_retrieve(ctx, dnsqr);
+		if (query == NULL) {
+			/* no corresponding query, this is an unsolicited response */
+			dnsqr->type = NMSG__ISC__DNS_QRTYPE__UDP_UNSOLICITED_RESPONSE;
+			*m = dnsqr_to_message(dnsqr);
+			if (*m == NULL) {
+				res = nmsg_res_memfail;
+				goto out;
+			}
+			res = nmsg_res_success;
+		} else {
+			/* corresponding query, merge query and response */
+			dnsqr->type = NMSG__ISC__DNS_QRTYPE__UDP_QUERY_RESPONSE;
+			dnsqr_merge(query, dnsqr);
+			*m = dnsqr_to_message(dnsqr);
+			if (*m == NULL) {
+				res = nmsg_res_memfail;
+				goto out;
+			}
+			res = nmsg_res_success;
+		}
 	}
-
-	*m = dnsqr_to_message(dnsqr);
-	if (*m == NULL)
-		res = nmsg_res_memfail;
 
 out:
 	if (dnsqr != NULL)
@@ -389,9 +685,12 @@ out:
 	return (res);
 }
 
-static nmsg_res
+nmsg_res
 dnsqr_pkt_to_payload(void *clos, nmsg_pcap_t pcap, nmsg_message_t *m) {
+	dnsqr_ctx_t *ctx = (dnsqr_ctx_t *) clos;
 	nmsg_res res;
+
+	//fprintf(stderr, "%s: ctx=%p\n", __func__, ctx);
 
 	/* XXX
 	 * expire outstanding queries
@@ -406,12 +705,38 @@ dnsqr_pkt_to_payload(void *clos, nmsg_pcap_t pcap, nmsg_message_t *m) {
 
 		res = nmsg_pcap_input_read_raw(pcap, &pkt_hdr, &pkt_data, &ts);
 		if (res == nmsg_res_success) {
-			return (do_packet(pcap, m, pkt_data, pkt_hdr));
+			return (do_packet(ctx, pcap, m, pkt_data, pkt_hdr));
 		} else if (res == nmsg_res_again) {
 			continue;
+		} else if (res == nmsg_res_eof) {
+			size_t n;
+			for (n = 0; n < ctx->num_slots; n++) {
+				hash_entry_t *he = &ctx->entries[n];
+				if (he->dnsqr != NULL) {
+					*m = dnsqr_to_message(he->dnsqr);
+					nmsg__isc__dns_qr__free_unpacked(he->dnsqr, NULL);
+					he->dnsqr = NULL;
+					return (nmsg_res_success);
+				}
+			}
+			return (res);
 		} else {
 			return (res);
 		}
 	}
 	return (res);
 }
+
+/* Export. */
+
+struct nmsg_msgmod_plugin nmsg_msgmod_ctx = {
+	.msgver = NMSG_MSGMOD_VERSION,
+	.vendor = NMSG_VENDOR_ISC,
+	.msgtype = { NMSG_VENDOR_ISC_DNSQR_ID, NMSG_VENDOR_ISC_DNSQR_NAME },
+
+	.pbdescr = &nmsg__isc__dns_qr__descriptor,
+	.fields = dnsqr_fields,
+	.init = dnsqr_init,
+	.fini = dnsqr_fini,
+	.pkt_to_payload = dnsqr_pkt_to_payload,
+};
