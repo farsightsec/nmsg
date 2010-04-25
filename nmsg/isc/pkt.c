f17e707cdd0752f9cb161ba9337a03572d5238bd
Robert Edmonds <edmonds@isc.org>
2010-04-25 17:27:20 -0400

    nmsg/isc/pkt: pkt_pkt_to_payload(): clarify control flow

--- nmsg/isc/pkt.c
+++ nmsg/isc/pkt.c
@@ -53,57 +53,48 @@ struct nmsg_msgmod_plugin nmsg_msgmod_ctx = {
 
 static nmsg_res
 pkt_pkt_to_payload(void *clos, nmsg_pcap_t pcap, nmsg_message_t *m) {
+	Nmsg__Isc__Pkt *pkt;
+	const uint8_t *pkt_data;
+	int snaplen;
 	nmsg_res res;
+	size_t buf_sz;
+	struct pcap_pkthdr *pkt_hdr;
+	struct timespec ts;
+	uint8_t *buf;
 
 	/* get a packet and return it as an encapsulated message object */
-	res = nmsg_res_failure;
-	while (res != nmsg_res_success) {
-		struct timespec ts;
-		struct pcap_pkthdr *pkt_hdr;
-		const uint8_t *pkt_data;
+	res = nmsg_pcap_input_read_raw(pcap, &pkt_hdr, &pkt_data, &ts);
+	if (res != nmsg_res_success)
+		return (res);
 
-		res = nmsg_pcap_input_read_raw(pcap, &pkt_hdr, &pkt_data, &ts);
-		if (res == nmsg_res_success) {
-			Nmsg__Isc__Pkt *pkt;
-			int snaplen;
-			size_t buf_sz;
-			uint8_t *buf;
+	/* get snaplen */
+	snaplen = nmsg_pcap_snapshot(pcap);
+	if (snaplen == 0)
+		snaplen = 65535;
 
-			/* get snaplen */
-			snaplen = nmsg_pcap_snapshot(pcap);
-			if (snaplen == 0)
-				snaplen = 65536;
+	/* allocate space for serialized payload */
+	buf = malloc(snaplen + 64);
+	if (buf == NULL)
+		return (nmsg_res_memfail);
 
-			/* allocate space for serialized payload */
-			buf = malloc(snaplen + 64);
-			if (buf == NULL)
-				return (nmsg_res_memfail);
-
-			/* initialize the Nmsg__Isc__Pkt object */
-			pkt = calloc(1, sizeof(*pkt));
-			if (pkt == NULL) {
-				free(buf);
-				return (nmsg_res_memfail);
-			}
-			nmsg__isc__pkt__init(pkt);
-
-			pkt->payload.len = pkt_hdr->caplen;
-			pkt->payload.data = (uint8_t *) pkt_data;
-			pkt->len_frame = pkt_hdr->len;
-			pkt->has_len_frame = 1;
-			buf_sz = nmsg__isc__pkt__pack(pkt, buf);
-			pkt->payload.len = 0;
-			pkt->payload.data = NULL;
-			*m = nmsg_message_from_raw_payload(NMSG_VENDOR_ISC_ID,
-							   NMSG_VENDOR_ISC_PKT_ID,
-							   buf, buf_sz, &ts);
-			free(pkt);
-			return (nmsg_res_success);
-		} else if (res == nmsg_res_again) {
-			continue;
-		} else {
-			return (res);
-		}
+	/* initialize the Nmsg__Isc__Pkt object */
+	pkt = calloc(1, sizeof(*pkt));
+	if (pkt == NULL) {
+		free(buf);
+		return (nmsg_res_memfail);
 	}
-	return (res);
+	nmsg__isc__pkt__init(pkt);
+
+	pkt->payload.len = pkt_hdr->caplen;
+	pkt->payload.data = (uint8_t *) pkt_data;
+	pkt->len_frame = pkt_hdr->len;
+	pkt->has_len_frame = 1;
+	buf_sz = nmsg__isc__pkt__pack(pkt, buf);
+	pkt->payload.len = 0;
+	pkt->payload.data = NULL;
+	*m = nmsg_message_from_raw_payload(NMSG_VENDOR_ISC_ID,
+					   NMSG_VENDOR_ISC_PKT_ID,
+					   buf, buf_sz, &ts);
+	free(pkt);
+	return (nmsg_res_success);
 }
