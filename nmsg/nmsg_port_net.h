e92db09173d6cadfd0fe6aa1c8164309fb886170
Robert Edmonds <edmonds@isc.org>
2010-09-07 23:12:42 -0400

    nmsg_port_net: network header structures

--- nmsg/nmsg_port_net.h
+++ nmsg/nmsg_port_net.h
@@ -1,81 +1,35 @@
 #ifndef NMSG_PORT_NET_H
 #define NMSG_PORT_NET_H
 
-#ifdef HAVE_SYS_PARAM_H
-# include <sys/param.h>
-#endif
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netinet/ip6.h>
+#include <stdint.h>
 
-#ifdef HAVE_SYS_TYPES_H
-# include <sys/types.h>
-#endif
+#include "config.h"
+#include "nmsg_port.h"
 
-#ifdef HAVE_SYS_SOCKET_H
-# include <sys/socket.h>
-#endif
-
-#ifdef HAVE_NETINET_IN_SYSTM_H
-# include <netinet/in_systm.h>
-#endif
-
-#ifdef HAVE_NETINET_IN_H
-# include <netinet/in.h>
-#endif
-
-#ifdef HAVE_NETINET_IP_H
-# include <netinet/ip.h>
-#endif
-
-#ifdef HAVE_NETINET_IP6_H
-# include <netinet/ip6.h>
-#endif
-
-#ifdef HAVE_NETINET_UDP_H
-# include <netinet/udp.h>
-#endif
-
-#ifdef HAVE_NETINET_TCP_H
-# include <netinet/tcp.h>
-#endif
-
-#ifdef HAVE_NETINET_IP_ICMP_H
-# include <netinet/ip_icmp.h>
-#endif
-
-#ifdef HAVE_NET_IF_H
-# include <net/if.h>
-#endif
-
-#ifdef HAVE_NET_ETHERNET_H
-# include <net/ethernet.h>
-#endif
-
-#ifdef HAVE_NET_ETHERTYPES_H
-# include <net/ethertypes.h>
-#endif
-
-#ifdef HAVE_NET_IF_ETHER_H
-# include <net/if_ether.h>
-#endif
-
-#ifdef HAVE_NETINET_IF_ETHER_H
-# include <netinet/if_ether.h>
-#endif
-
-#ifdef HAVE_ARPA_INET_H
-# include <arpa/inet.h>
-#endif
-
-#ifdef __APPLE__
-# include <sys/ioctl.h>
-# define BIOCIMMEDIATE _IOW('B',112, u_int)
-#endif
 
 #ifndef ETHER_HDR_LEN
 # define ETHER_HDR_LEN 14
 #endif
 
+#ifndef ETH_ALEN
+# define ETH_ALEN 6
+#endif
+
 #ifndef IP_OFFMASK
 # define IP_OFFMASK 0x1fff
+#endif
+
+#ifndef IP_DF
+# define IP_DF 0x4000
+#endif
+
+#ifndef IP_MF
+# define IP_MF 0x2000
 #endif
 
 #ifndef ETHERTYPE_IP
@@ -97,5 +51,61 @@
 #ifndef IPV6_VERSION_MASK
 # define IPV6_VERSION_MASK 0xf0
 #endif
+
+struct nmsg_ethhdr {
+	uint8_t		ether_dhost[ETH_ALEN];
+	uint8_t		ether_shost[ETH_ALEN];
+	uint16_t	ether_type;
+} __attribute__ ((__packed__));
+
+struct nmsg_iphdr {
+#if IS_LITTLE_ENDIAN
+	unsigned int	ip_hl:4;
+	unsigned int	ip_v:4;
+#else
+	unsigned int	ip_v:4;
+	unsigned int	ip_hl:4;
+#endif
+	uint8_t		ip_tos;
+	uint16_t	ip_len;
+	uint16_t	ip_id;
+	uint16_t	ip_off;
+	uint8_t		ip_ttl;
+	uint8_t		ip_p;
+	uint16_t	ip_sum;
+	struct in_addr	ip_src;
+	struct in_addr	ip_dst;
+} __attribute__ ((__packed__));
+
+struct nmsg_tcphdr {
+	uint16_t	th_sport;
+	uint16_t	th_dport;
+	uint32_t	th_seq;
+	uint32_t	th_ack;
+#if IS_LITTLE_ENDIAN
+	uint8_t		th_x2:4;
+	uint8_t		th_off:4;
+#else
+	uint8_t		th_off:4;
+	uint8_t		th_x2:4;
+#endif
+	uint8_t		th_flags;
+	uint16_t	th_win;
+	uint16_t	th_sum;
+	uint16_t	th_urp;
+} __attribute__ ((__packed__));
+
+struct nmsg_udphdr {
+	uint16_t	uh_sport;
+	uint16_t	uh_dport;
+	uint16_t	uh_ulen;
+	uint16_t	uh_sum;
+} __attribute__ ((__packed__));
+
+struct nmsg_icmphdr {
+	uint8_t		icmp_type;
+	uint8_t		icmp_code;
+	uint16_t	icmp_cksum;
+} __attribute__ ((__packed__));
 
 #endif /* NMSG_PORT_NET_H */
