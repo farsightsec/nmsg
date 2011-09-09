37e7ca2f7b6d63d03c45393cc4bd9569d6a7192e
Robert Edmonds <edmonds@isc.org>
2011-09-08 20:58:57 -0400

    tests/payload-crc32c-tests/: test both "-o" and "-w" code paths

--- tests/payload-crc32c-tests/test.sh
+++ tests/payload-crc32c-tests/test.sh
@@ -8,7 +8,15 @@ ERR="^libnmsg: WARNING: crc mismatch"
 # test vectors
 $CRC32C_TEST
 
-n="CRC32C absent"
+n="CRC32C absent #1"
+x="test_crc32c_absent.nmsg"
+if $NMSGTOOL -r $x -w /dev/null 2>&1 | grep -q "$ERR"; then
+    echo "FAIL: $n"
+else
+    echo "PASS: $n"
+fi
+
+n="CRC32C absent #2"
 x="test_crc32c_absent.nmsg"
 if $NMSGTOOL -r $x -o /dev/null 2>&1 | grep -q "$ERR"; then
     echo "FAIL: $n"
@@ -16,7 +24,15 @@ else
     echo "PASS: $n"
 fi
 
-n="CRC32C regeneration"
+n="CRC32C regeneration #1"
+x="test_crc32c_absent.nmsg"
+if $NMSGTOOL -r $x -w - | $NMSGTOOL -r - -w /dev/null 2>&1 | grep -q "$ERR"; then
+    echo "FAIL: $n"
+else
+    echo "PASS: $n"
+fi
+
+n="CRC32C regeneration #2"
 x="test_crc32c_absent.nmsg"
 if $NMSGTOOL -r $x -w - | $NMSGTOOL -r - -o /dev/null 2>&1 | grep -q "$ERR"; then
     echo "FAIL: $n"
@@ -24,7 +40,15 @@ else
     echo "PASS: $n"
 fi
 
-n="CRC32C present and correct"
+n="CRC32C present and correct #1"
+x="test_crc32c_correct.nmsg"
+if $NMSGTOOL -r $x -w /dev/null 2>&1 | grep -q "$ERR"; then
+    echo "FAIL: $n"
+else
+    echo "PASS: $n"
+fi
+
+n="CRC32C present and correct #2"
 x="test_crc32c_correct.nmsg"
 if $NMSGTOOL -r $x -o /dev/null 2>&1 | grep -q "$ERR"; then
     echo "FAIL: $n"
@@ -32,7 +56,15 @@ else
     echo "PASS: $n"
 fi
 
-n="CRC32C present and incorrect"
+n="CRC32C present and incorrect #1"
+x="test_crc32c_incorrect.nmsg"
+if $NMSGTOOL -r test_crc32c_incorrect.nmsg -w /dev/null 2>&1 | grep -q "$ERR"; then
+    echo "PASS: $n"
+else
+    echo "FAIL: $n"
+fi
+
+n="CRC32C present and incorrect #2"
 x="test_crc32c_incorrect.nmsg"
 if $NMSGTOOL -r test_crc32c_incorrect.nmsg -o /dev/null 2>&1 | grep -q "$ERR"; then
     echo "PASS: $n"
