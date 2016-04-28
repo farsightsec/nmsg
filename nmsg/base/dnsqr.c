dd75d49452a1dcefd111351fb82b6228688aa25a
Robert Edmonds <edmonds@fsi.io>
2016-04-28 17:30:08 -0400

    dnsqr: Copyright years

--- nmsg/base/dnsqr.c
+++ nmsg/base/dnsqr.c
@@ -1,7 +1,7 @@
 /* dnsqr nmsg message module */
 
 /*
- * Copyright (c) 2010-2013, 2015 by Farsight Security, Inc.
+ * Copyright (c) 2010-2016 by Farsight Security, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
