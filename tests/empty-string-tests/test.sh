5010149743a6a2143e86b49022020a121784d8d3
Chris Mikkelson <cmikk@fsi.io>
2017-10-20 17:45:52 -0500

    Clean up empty string test scripts
      * set up test environment variables in Makefile.am
      * correct nmsgtool and reference file paths
      * continue tests after failure
      * add test files and script to distribution

--- tests/empty-string-tests/test.sh
+++ tests/empty-string-tests/test.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
 
+if [ -z "${top_srcdir}" ]; then
+    echo "top_srcdir variable not set"
+    exit 1
+fi
+
+if [ -z "${top_builddir}" ]; then
+    echo "top_builddir variable not set"
+    exit 1
+fi
+
 NMSG_MSGMOD_DIR="${top_builddir}/nmsg/base/.libs"
 export NMSG_MSGMOD_DIR
 
 NMSGTOOL="${top_builddir}/src/nmsgtool"
-TJSON="${top_srcdir}/t/empty-string-tests/empty-string.json"
-TNMSG="${top_srcdir}/t/empty-string-tests/empty-string.nmsg"
+TJSON="${top_srcdir}/tests/empty-string-tests/empty-string.json"
+TNMSG="${top_srcdir}/tests/empty-string-tests/empty-string.nmsg"
+
+status=0
 
 if $NMSGTOOL -r $TNMSG | fgrep "(null)"; then
     echo FAIL: presentation contains '"(null)"'
+    status=1
 else
-    echo PASS:
+    echo PASS: presentation format empty
 fi
 
-if $NMSGTOOL -j $TJSON | cmp - $TMSG; then
-    echo FAIL: json load failed
+if $NMSGTOOL -j $TJSON -w - | cmp - $TNMSG; then
+    echo PASS: json load succeeded
 else
-    echo PASS:
+    echo FAIL: json load failed
+    status=1
 fi
+
+exit $status
