b5517c3415b3136e85ff229bc71b2464c3dcd9d8
Chris Mikkelson <cmikk@fsi.io>
2017-11-02 12:14:00 -0500

    Revert "Correct presentation output for empty string fields."
    This reverts commit e3e4e66d2cd85afff614897f9815c744bf1e11b6.
    
    The commit only covered the case of a NULL bdata->data pointer,
    by checking if bdata->len == 0. It did not handle the case of
    a string field with missing NUL terminator.

--- nmsg/msgmod/transparent_payload.c
+++ nmsg/msgmod/transparent_payload.c
@@ -163,7 +163,7 @@ _nmsg_message_payload_to_pres_load(struct nmsg_message *msg,
 		bdata = (ProtobufCBinaryData *) ptr;
 		nmsg_strbuf_append(sb, "%s: %s%s",
 				   field->name,
-				   (bdata->len > 0)?bdata->data:(uint8_t *)"",
+				   bdata->data,
 				   endline);
 		break;
 	case nmsg_msgmod_ft_mlstring:
@@ -171,7 +171,7 @@ _nmsg_message_payload_to_pres_load(struct nmsg_message *msg,
 		nmsg_strbuf_append(sb, "%s:%s%s.%s",
 				   field->name,
 				   endline,
-				   (bdata->len > 0)?bdata->data:(uint8_t *)"",
+				   bdata->data,
 				   endline);
 		break;
 	case nmsg_msgmod_ft_bool: {
