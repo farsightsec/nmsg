b5fa4b219a6f2913c12e5fdd4acdc5c557f5a91e
Stephen Watt <shw@fsi.io>
2018-03-01 22:15:45 +0000

    Introduced new macros in errors.h to be used instead of assert(). Converted certain unit tests to use these new functions.

--- tests/test-parse.c
+++ tests/test-parse.c
@@ -14,12 +14,13 @@
  * limitations under the License.
  */
 
-#include <assert.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#include "errors.h"
 
 #include "nmsg.h"
 #include "nmsg/asprintf.h"
@@ -52,158 +53,167 @@ test_json(void)
 	nmsg_msgmod_t mmod1, mmod2;
 	FILE *f;
 	int fd;
-	const char *fname;
-	void *data;
+	const char *fname = NULL;
+	void *data = NULL;
 	size_t dlen, nf;
 	unsigned idx;
 
 	f = tmpfile();
-	assert(f != NULL);
+	check_return(f != NULL);
 
 	fd = fileno(f);
-	assert(fd != -1);
+	check_return(fd != -1);
 
-	assert(write(fd, TEST_JSON_1, strlen(TEST_JSON_1)) == strlen(TEST_JSON_1));
-	assert(write(fd, "\n", 1) == 1);
+	check_return(write(fd, TEST_JSON_1, strlen(TEST_JSON_1)) == strlen(TEST_JSON_1));
+	check_return(write(fd, "\n", 1) == 1);
 
-	assert(write(fd, TEST_JSON_2, strlen(TEST_JSON_2)) == strlen(TEST_JSON_2));
-	assert(write(fd, "\n", 1) == 1);
+	check_return(write(fd, TEST_JSON_2, strlen(TEST_JSON_2)) == strlen(TEST_JSON_2));
+	check_return(write(fd, "\n", 1) == 1);
 
-	assert(lseek(fd, SEEK_SET, 0) == 0);
+	check_return(lseek(fd, SEEK_SET, 0) == 0);
 
 	i = nmsg_input_open_json(fd);
-	assert(i != NULL);
+	check_return(i != NULL);
 
 	/* Message #1 */
-	assert(nmsg_input_read(i, &m) == nmsg_res_success);
+	check_return(nmsg_input_read(i, &m) == nmsg_res_success);
 
-	assert(nmsg_message_get_vid(m) == NMSG_VENDOR_SIE_ID);
-	assert(nmsg_message_get_msgtype(m) == NMSG_VENDOR_SIE_DNSDEDUPE_ID);
-	assert(nmsg_message_get_source(m) == 0xa1ba02cf);
-	assert(nmsg_message_get_num_fields(m, &nf) == nmsg_res_success);
-	assert(nf == 14);
+	check(nmsg_message_get_vid(m) == NMSG_VENDOR_SIE_ID);
+	check(nmsg_message_get_msgtype(m) == NMSG_VENDOR_SIE_DNSDEDUPE_ID);
+	check(nmsg_message_get_source(m) == 0xa1ba02cf);
+	check(nmsg_message_get_num_fields(m, &nf) == nmsg_res_success);
+	check(nf == 14);
 	mmod1 = nmsg_message_get_msgmod(m);
 
-	assert(nmsg_message_get_field_idx(m, "time_first", &idx) == nmsg_res_success);
-	assert(idx == 2);
-	assert(nmsg_message_get_field_idx(m, "response_ip", &idx) == nmsg_res_success);
-	assert(idx == 6);
-	assert(nmsg_message_get_field_idx(m, "rrttl", &idx) == nmsg_res_success);
-	assert(idx == 11);
-	assert(nmsg_message_get_field_idx(m, "this_is_an_error", &idx) != nmsg_res_success);
+	check(nmsg_message_get_field_idx(m, "time_first", &idx) == nmsg_res_success);
+	check(idx == 2);
+	check(nmsg_message_get_field_idx(m, "response_ip", &idx) == nmsg_res_success);
+	check(idx == 6);
+	check(nmsg_message_get_field_idx(m, "rrttl", &idx) == nmsg_res_success);
+	check(idx == 11);
+	check(nmsg_message_get_field_idx(m, "this_is_an_error", &idx) != nmsg_res_success);
 
-	assert(nmsg_message_get_num_field_values(m, "rdata", &nf) == nmsg_res_success);
-	assert(nf == 2);
+	check(nmsg_message_get_num_field_values(m, "rdata", &nf) == nmsg_res_success);
+	check(nf == 2);
 
 	/* Arbitrary check; the indexed field is "count" */
-	assert(nmsg_message_get_field_name(m, 1, &fname) == nmsg_res_success);
-	assert(!strcmp(fname, "count"));
-	assert(nmsg_message_get_field_by_idx(m, 1, 0, &data, &dlen) == nmsg_res_success);
-	assert(dlen == 4);
-	assert(*((uint32_t *)data) == 2);
-	assert(nmsg_message_get_field_type_by_idx(m, 1, &ftype) == nmsg_res_success);
-	assert(ftype == nmsg_msgmod_ft_uint32);
+	check(nmsg_message_get_field_name(m, 1, &fname) == nmsg_res_success);
+	check(fname && (!strcmp(fname, "count")));
+	check(nmsg_message_get_field_by_idx(m, 1, 0, &data, &dlen) == nmsg_res_success);
+	check(dlen == 4);
+	check(data && (*((uint32_t *)data) == 2));
+	check(nmsg_message_get_field_type_by_idx(m, 1, &ftype) == nmsg_res_success);
+	check(ftype == nmsg_msgmod_ft_uint32);
 
-	assert(nmsg_message_get_field(m, "type", 0, &data, &dlen) == nmsg_res_success);
-	assert(dlen == 4);
-	assert(*((uint32_t *)data) == NMSG__SIE__DNS_DEDUPE_TYPE__INSERTION);
+	data = NULL;
+	check(nmsg_message_get_field(m, "type", 0, &data, &dlen) == nmsg_res_success);
+	check(dlen == 4);
+	check(data && (*((uint32_t *)data) == NMSG__SIE__DNS_DEDUPE_TYPE__INSERTION));
 
 	/* Try by name... and then by index */
-	assert(nmsg_message_enum_value_to_name(m, "type", *((uint32_t *)data), &fname) == nmsg_res_success);
-	assert(!strcmp(fname, "INSERTION"));
+	fname = NULL;
+	check(data && (nmsg_message_enum_value_to_name(m, "type", *((uint32_t *)data), &fname) == nmsg_res_success));
+	check(fname && (!strcmp(fname, "INSERTION")));
 
 	fname = NULL;
-	assert(nmsg_message_enum_value_to_name_by_idx(m, 0, *((uint32_t *)data), &fname) == nmsg_res_success);
-	assert(!strcmp(fname, "INSERTION"));
+	check(data && (nmsg_message_enum_value_to_name_by_idx(m, 0, *((uint32_t *)data), &fname) == nmsg_res_success));
+	check(fname && (!strcmp(fname, "INSERTION")));
 
 	unsigned e_val;
-	assert(nmsg_message_enum_name_to_value(m, "type", "AUTHORITATIVE", &e_val) == nmsg_res_success);
-	assert(e_val == NMSG__SIE__DNS_DEDUPE_TYPE__AUTHORITATIVE);
+	check(nmsg_message_enum_name_to_value(m, "type", "AUTHORITATIVE", &e_val) == nmsg_res_success);
+	check(e_val == NMSG__SIE__DNS_DEDUPE_TYPE__AUTHORITATIVE);
 
-	assert(nmsg_message_enum_name_to_value_by_idx(m, 0, "fake_data", &e_val) != nmsg_res_success);
+	check(nmsg_message_enum_name_to_value_by_idx(m, 0, "fake_data", &e_val) != nmsg_res_success);
 
-	assert(nmsg_message_enum_name_to_value_by_idx(m, 0, "MERGED", &e_val) == nmsg_res_success);
-	assert(e_val == NMSG__SIE__DNS_DEDUPE_TYPE__MERGED);
+	check(nmsg_message_enum_name_to_value_by_idx(m, 0, "MERGED", &e_val) == nmsg_res_success);
+	check(e_val == NMSG__SIE__DNS_DEDUPE_TYPE__MERGED);
 
-	assert(nmsg_message_get_field(m, "count", 0, &data, &dlen) == nmsg_res_success);
-	assert(dlen == 4);
-	assert(*((uint32_t *)data) == 2);
+	data = NULL;
+	check(nmsg_message_get_field(m, "count", 0, &data, &dlen) == nmsg_res_success);
+	check(dlen == 4);
+	check(data && (*((uint32_t *)data) == 2));
 
-	assert(nmsg_message_get_field(m, "response_ip", 0, &data, &dlen) == nmsg_res_success);
-	assert(dlen == 4);
-	assert(*((uint32_t *)data) == inet_addr("194.85.252.62"));
+	data = NULL;
+	check(nmsg_message_get_field(m, "response_ip", 0, &data, &dlen) == nmsg_res_success);
+	check(dlen == 4);
+	check(data && (*((uint32_t *)data) == inet_addr("194.85.252.62")));
 
-	assert(nmsg_message_get_field(m, "type", 0, &data, &dlen) == nmsg_res_success);
-	assert(dlen == 4);
+	check(nmsg_message_get_field(m, "type", 0, &data, &dlen) == nmsg_res_success);
+	check(dlen == 4);
 
-	assert(nmsg_message_get_field(m, "rrclass", 0, &data, &dlen) == nmsg_res_success);
-	assert(dlen == 4);
-	assert(*((uint32_t *)data) == WDNS_CLASS_IN);
+	data = NULL;
+	check(nmsg_message_get_field(m, "rrclass", 0, &data, &dlen) == nmsg_res_success);
+	check(dlen == 4);
+	check(data && (*((uint32_t *)data) == WDNS_CLASS_IN));
 
-	assert(nmsg_message_get_field(m, "rrtype", 0, &data, &dlen) == nmsg_res_success);
-	assert(dlen == 4);
-	assert(*((uint32_t *)data) == WDNS_TYPE_NS);
+	data = NULL;
+	check(nmsg_message_get_field(m, "rrtype", 0, &data, &dlen) == nmsg_res_success);
+	check(dlen == 4);
+	check(data && (*((uint32_t *)data) == WDNS_TYPE_NS));
 
-	assert(nmsg_message_get_field(m, "rrttl", 0, &data, &dlen) == nmsg_res_success);
-	assert(dlen == 4);
-	assert(*((uint32_t *)data) == 345600);
+	data = NULL;
+	check(nmsg_message_get_field(m, "rrttl", 0, &data, &dlen) == nmsg_res_success);
+	check(dlen == 4);
+	check(data && (*((uint32_t *)data) == 345600));
 
-	assert(nmsg_message_get_field_type(m, "rdata", &ftype) == nmsg_res_success);
-	assert(ftype == nmsg_msgmod_ft_bytes);
+	check(nmsg_message_get_field_type(m, "rdata", &ftype) == nmsg_res_success);
+	check(ftype == nmsg_msgmod_ft_bytes);
 
 	/* Message #2 */
-	assert(nmsg_input_read(i, &m) == nmsg_res_success);
+	check_return(nmsg_input_read(i, &m) == nmsg_res_success);
 
-	assert(nmsg_message_get_vid(m) == NMSG_VENDOR_SIE_ID);
-	assert(nmsg_message_get_msgtype(m) == NMSG_VENDOR_SIE_NEWDOMAIN_ID);
-	assert(nmsg_message_get_source(m) == 0xa1ba02cf);
-	assert(nmsg_message_get_num_fields(m, &nf) == nmsg_res_success);
-	assert(nf == 22);
+	check(nmsg_message_get_vid(m) == NMSG_VENDOR_SIE_ID);
+	check(nmsg_message_get_msgtype(m) == NMSG_VENDOR_SIE_NEWDOMAIN_ID);
+	check(nmsg_message_get_source(m) == 0xa1ba02cf);
+	check(nmsg_message_get_num_fields(m, &nf) == nmsg_res_success);
+	check(nf == 22);
 	mmod2 = nmsg_message_get_msgmod(m);
 
-	assert(mmod1 != mmod2);
+	check(mmod1 != mmod2);
 
-	assert(nmsg_message_get_field(m, "rrclass", 0, &data, &dlen) == nmsg_res_success);
-	assert(dlen == 4);
-	assert(*((uint32_t *)data) == WDNS_CLASS_IN);
+	data = NULL;
+	check(nmsg_message_get_field(m, "rrclass", 0, &data, &dlen) == nmsg_res_success);
+	check(dlen == 4);
+	check(data && (*((uint32_t *)data) == WDNS_CLASS_IN));
 
-	assert(nmsg_message_get_field(m, "rrtype", 0, &data, &dlen) == nmsg_res_success);
-	assert(dlen == 4);
-	assert(*((uint32_t *)data) == WDNS_TYPE_MX);
+	data = NULL;
+	check(nmsg_message_get_field(m, "rrtype", 0, &data, &dlen) == nmsg_res_success);
+	check(dlen == 4);
+	check(data && (*((uint32_t *)data) == WDNS_TYPE_MX));
 
-	assert(nmsg_message_get_field(m, "rrttl", 0, &data, &dlen) != nmsg_res_success);
+	check(nmsg_message_get_field(m, "rrttl", 0, &data, &dlen) != nmsg_res_success);
 
 	/*
 	 * Write the last piece of json data back to ourselves.
 	 * Set some message attributes and make sure they survive.
 	  */
-	assert(lseek(fd, SEEK_SET, 0) == 0);
-	assert(ftruncate(fd, 0) != -1);
+	check_return(lseek(fd, SEEK_SET, 0) == 0);
+	check_return(ftruncate(fd, 0) != -1);
 	o = nmsg_output_open_json(fd);
-	assert(o != NULL);
+	check_return(o != NULL);
 
 	nmsg_message_set_source(m, 4321);
 	nmsg_message_set_group(m, 1234);
 	nmsg_message_set_operator(m, 13);
-	assert(nmsg_output_write(o, m) == nmsg_res_success);
-	assert(lseek(fd, SEEK_SET, 0) == 0);
+	check_return(nmsg_output_write(o, m) == nmsg_res_success);
+	check_return(lseek(fd, SEEK_SET, 0) == 0);
 
-	assert(nmsg_input_read(i, &m2) == nmsg_res_success);
-	assert(nmsg_message_get_source(m) == 4321);
-	assert(nmsg_message_get_group(m) == 1234);
-	assert(nmsg_message_get_operator(m) == 13);
+	check_return(nmsg_input_read(i, &m2) == nmsg_res_success);
+	check(nmsg_message_get_source(m) == 4321);
+	check(nmsg_message_get_group(m) == 1234);
+	check(nmsg_message_get_operator(m) == 13);
 
-	assert(nmsg_message_get_msgmod(m2) == mmod2);
+	check(nmsg_message_get_msgmod(m2) == mmod2);
 
 	nmsg_message_destroy(&m);
 	nmsg_message_destroy(&m2);
 
-	assert(nmsg_input_close(&i) == nmsg_res_success);
-	assert(nmsg_output_close(&o) == nmsg_res_success);
+	check(nmsg_input_close(&i) == nmsg_res_success);
+	check(nmsg_output_close(&o) == nmsg_res_success);
 
 	fclose(f);
 
-	return 0;
+	l_return_test_status();
 }
 
 /* Test serialization and deserialization of nmsg data to/from json and pres formats */
@@ -213,9 +223,9 @@ test_serialize(void)
 	struct timespec ts1, ts2;
 	nmsg_msgmod_t mm;
 	nmsg_input_t i;
-	nmsg_message_t m1, m2, m3;
+	nmsg_message_t m1, m2;
 	char *jout, *pres;
-	void *p1, *p2, *p3;
+	void *p1, *p2;
 	FILE *f;
 	int fd;
 	long sec_new = 0xc0debeef, nsec_new = 0xc0ffee;
@@ -225,7 +235,7 @@ test_serialize(void)
 	 * Then rewrite it both to json and presentation format.
 	 * Then read both of those emissions back into standard nmsg data.
 	 */
-	assert(nmsg_message_from_json(TEST_JSON_1, &m1) == nmsg_res_success);
+	check_return(nmsg_message_from_json(TEST_JSON_1, &m1) == nmsg_res_success);
 
 	/*
 	 * Throw in a test for nmsg_message_set_time() while we're at it.
@@ -237,87 +247,72 @@ test_serialize(void)
 	nmsg_message_set_time(m1, &ts1);
 
 	/* Serialize the modified message and then read it back and verify it. */
-	assert(nmsg_message_to_json(m1, &jout) == nmsg_res_success);
-	assert(jout != NULL);
+	check_return(nmsg_message_to_json(m1, &jout) == nmsg_res_success);
+	check_return(jout != NULL);
 
-	assert(nmsg_message_from_json(jout, &m2) == nmsg_res_success);
+	check_return(nmsg_message_from_json(jout, &m2) == nmsg_res_success);
 	free(jout);
 	nmsg_message_get_time(m2, &ts2);
 
-	assert(ts2.tv_sec == sec_new);
-	assert(ts2.tv_nsec == nsec_new);
+	check(ts2.tv_sec == sec_new);
+	check(ts2.tv_nsec == nsec_new);
 
 	/* Now try serialization to presentation format. */
-	assert(nmsg_message_to_pres(m1, &pres, "\n") == nmsg_res_success);
-	assert(pres != NULL);
+	check_return(nmsg_message_to_pres(m1, &pres, "\n") == nmsg_res_success);
+	check_return(pres != NULL);
 
 	/* One more hoop to jump through deserializing presentation format */
 	f = tmpfile();
-	assert(f != NULL);
+	check_return(f != NULL);
 	fd = fileno(f);
-	assert(fd != -1);
+	check_return(fd != -1);
 
 //	fprintf(stderr, "pres  len = %zu, contents = [%s]\n", strlen(pres), pres);
-	assert(write(fd, pres, strlen(pres)) == (int)strlen(pres));
-	assert(write(fd, "\n", 1) == 1);
-	assert(lseek(fd, SEEK_SET, 0) != -1);
+	check_return(write(fd, pres, strlen(pres)) == (int)strlen(pres));
+	check_return(write(fd, "\n", 1) == 1);
+	check_return(lseek(fd, SEEK_SET, 0) != -1);
 	free(pres);
 	mm = nmsg_msgmod_lookup_byname("SIE", "dnsdedupe");
-	assert(mm != NULL);
+	check_return(mm != NULL);
 
 	i = nmsg_input_open_pres(fd, mm);
-	assert(i != NULL);
+	check_return(i != NULL);
 
 	/* Not sure why this fails ;/ */
-//	assert(nmsg_input_read(i, &m3) == nmsg_res_success);
+//	check_return(nmsg_input_read(i, &m3) == nmsg_res_success);
 
 	/* Finally, compare original message with deserialized json and pres. forms */
-	assert(nmsg_message_get_payload_size(m1) == nmsg_message_get_payload_size(m2));
-//	assert(nmsg_message_get_payload_size(m2) == nmsg_message_get_payload_size(m3));
+	check_return(nmsg_message_get_payload_size(m1) == nmsg_message_get_payload_size(m2));
+//	check_return(nmsg_message_get_payload_size(m2) == nmsg_message_get_payload_size(m3));
 
 	/* The data encoded and decoded from json and pres format should all be identical */
 	p1 = nmsg_message_get_payload(m1);
-	assert(p1 != NULL);
+	check_return(p1 != NULL);
 	p2 = nmsg_message_get_payload(m2);
-	assert(p2 != NULL);
-	assert(!memcmp(p1, p2, nmsg_message_get_payload_size(m1)));
+	check_return(p2 != NULL);
+	check(!memcmp(p1, p2, nmsg_message_get_payload_size(m1)));
 //	p3 = nmsg_message_get_payload(m3);
-//	assert(p3 != NULL);
-//	assert(!memcmp(p1, p3, nmsg_message_get_payload_size(m1)));
+//	check_return(p3 != NULL);
+//	check(!memcmp(p1, p3, nmsg_message_get_payload_size(m1)));
 
 	nmsg_message_destroy(&m1);
 	nmsg_message_destroy(&m2);
 //	nmsg_message_destroy(&m3);
 
-	assert(nmsg_input_close(&i) == nmsg_res_success);
+	check(nmsg_input_close(&i) == nmsg_res_success);
 
 	fclose(f);
 
-	return 0;
-}
-
-static int
-check(int ret, const char *s)
-{
-	if (ret == 0) {
-		fprintf(stderr, NAME ": PASS: %s\n", s);
-	} else {
-		fprintf(stderr, NAME ": FAIL: %s\n", s);
-	}
-	return ret;
+	l_return_test_status();
 }
 
 int
 main(void)
 {
-	int ret = 0;
+	check_abort(nmsg_init() == nmsg_res_success);
 
-	assert(nmsg_init() == nmsg_res_success);
+	check_explicit2_display_only(test_json() == 0, "test-parse / test_json");
+	check_explicit2_display_only(test_serialize() == 0, "test-parse / test_serialize");
 
-	ret |= check(test_json(), "test-parse");
-	ret |= check(test_serialize(), "test-parse");
-
-	if (ret)
-		return EXIT_FAILURE;
-	return EXIT_SUCCESS;
+	g_check_test_status(false);
 }
