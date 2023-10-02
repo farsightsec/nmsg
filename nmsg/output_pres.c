919ce283f84f0aadfb262463ef1d709b1f0ffe60
Stephen Watt <swatt@domaintools.com>
2023-10-02 19:52:24 +0000

    Always print out un-resolvable group and operator values in presentation mode. Ensure mis-running nmsgtool results in failure exit code.

--- nmsg/output_pres.c
+++ nmsg/output_pres.c
@@ -23,10 +23,11 @@
 nmsg_res
 _output_pres_write(nmsg_output_t output, nmsg_message_t msg) {
 	Nmsg__NmsgPayload *np;
-	const char *vname = NULL;
-	const char *mname = NULL;
-	char *pres_data;
+	char op_buf[sizeof("4294967295")] = {0}, group_buf[sizeof("4294967295")] = {0};
+	const char *op_str = op_buf, *group_str = group_buf;
+	const char *vname = NULL, *mname = NULL;
 	char when[32];
+	char *pres_data;
 	nmsg_msgmod_t mod;
 	nmsg_res res;
 	struct tm tm;
@@ -52,6 +53,25 @@ _output_pres_write(nmsg_output_t output, nmsg_message_t msg) {
 	}
 	vname = nmsg_msgmod_vid_to_vname(np->vid);
 	mname = nmsg_msgmod_msgtype_to_mname(np->vid, np->msgtype);
+
+	if (np->has_operator_) {
+		op_str = nmsg_alias_by_key(nmsg_alias_operator, np->operator_);
+
+		if (op_str == NULL) {
+			snprintf(op_buf, sizeof(op_buf), "%"PRIu32, np->operator_);
+			op_str = op_buf;
+		}
+	}
+
+	if (np->has_group) {
+		group_str = nmsg_alias_by_key(nmsg_alias_group, np->group);
+
+		if (group_str == NULL) {
+			snprintf(group_buf, sizeof(group_buf), "%"PRIu32, np->group);
+			group_str = group_buf;
+		}
+	}
+
 	fprintf(output->pres->fp, "[%zu] [%s.%09u] [%d:%d %s %s] "
 		"[%08x] [%s] [%s] %s%s",
 		np->has_payload ? np->payload.len : 0,
@@ -60,15 +80,8 @@ _output_pres_write(nmsg_output_t output, nmsg_message_t msg) {
 		vname ? vname : "(unknown)",
 		mname ? mname : "(unknown)",
 		np->has_source ? np->source : 0,
-
-		np->has_operator_ ?
-			nmsg_alias_by_key(nmsg_alias_operator, np->operator_)
-			: "",
-
-		np->has_group ?
-			nmsg_alias_by_key(nmsg_alias_group, np->group)
-			: "",
-
+		op_str,
+		group_str,
 		output->pres->endline, pres_data);
 	fputs("\n", output->pres->fp);
 	if (output->pres->flush)
