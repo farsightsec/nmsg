14897639b67d79a1b1cd89f2d1fc0a075a57ed8b
Stephen Watt <swatt@domaintools.com>
2024-06-11 12:18:17 -0400

    Private tests cleanup.

--- tests/test-private.c
+++ tests/test-private.c
@@ -37,68 +37,66 @@ typedef struct {
 } answer_t;
 
 typedef struct {
-	const char *question;
+	const char *payload;
 	const answer_t *answer;
 } task_t;
 
-/* A binary value generated from the base64 decoding of q2.message.response */
-const char a2_response[] = {0x5b,0x94,0x84,0x20,0x00,0x01,0x00,0x03,0x00,0x04,0x00,0x01,0x04,0x78,0x78,0x78,0x78,0x04,0x78,0x78,0x78,0x78,0x05,0x78,0x78,0x78,0x78,0x78,0x03,0x78,0x78,0x78,0x03,0x78,0x78,0x78,0x0b,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x03,0x78,0x78,0x78,0x00,0x00,0x01,0x00,0x01,0xc0,0x0c,0x00,0x01,0x00,0x01,0x00,0x00,0x00,0x3c,0x00,0x04,0x00,0x00,0x00,0x00,0xc0,0x0c,0x00,0x01,0x00,0x01,0x00,0x00,0x00,0x3c,0x00,0x04,0x00,0x00,0x00,0x00,0xc0,0x0c,0x00,0x01,0x00,0x01,0x00,0x00,0x00,0x3c,0x00,0x04,0x00,0x00,0x00,0x00,0xc0,0x16,0x00,0x02,0x00,0x01,0x00,0x02,0xa3,0x00,0x00,0x17,0x07,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x09,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x03,0x78,0x78,0x78,0x00,0xc0,0x16,0x00,0x02,0x00,0x01,0x00,0x02,0xa3,0x00,0x00,0x19,0x07,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x09,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x02,0x78,0x78,0x02,0x78,0x78,0x00,0xc0,0x16,0x00,0x02,0x00,0x01,0x00,0x02,0xa3,0x00,0x00,0x13,0x06,0x78,0x78,0x78,0x78,0x78,0x78,0x09,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0xc0,0x30,0xc0,0x16,0x00,0x02,0x00,0x01,0x00,0x02,0xa3,0x00,0x00,0x16,0x06,0x78,0x78,0x78,0x78,0x78,0x78,0x09,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x78,0x03,0x78,0x78,0x78,0x00,0x00,0x00,0x29,0x10,0x00,0x00,0x00,0x80,0x00,0x00,0x00};
- /* A binary value generated from the base64 decoding of q3.message.query_packet */
-const char a3_query_packet[] = {0x45,0x00,0x00,0x47,0x3e,0xc8,0x00,0x00,0x01,0x11,0xad,0xc3,0xac,0x1e,0x40,0x01,0xe0,0x00,0x00,0xfb,0x14,0xe9,0x14,0xe9,0x00,0x33,0x5f,0x1d,0x00,0x00,0x00,0x00,0x00,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x0e,0x5f,0x6d,0x69,0x63,0x72,0x6f,0x73,0x6f,0x66,0x74,0x5f,0x6d,0x63,0x63,0x04,0x5f,0x74,0x63,0x70,0x05,0x6c,0x6f,0x63,0x61,0x6c,0x00,0x00,0x0c,0x80,0x01};
-/* A binary value generated from the base64 decoding of q3.message.query */
-const char a3_query[] = {0x00,0x00,0x00,0x00,0x00,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x0e,0x5f,0x6d,0x69,0x63,0x72,0x6f,0x73,0x6f,0x66,0x74,0x5f,0x6d,0x63,0x63,0x04,0x5f,0x74,0x63,0x70,0x05,0x6c,0x6f,0x63,0x61,0x6c,0x00,0x00,0x0c,0x80,0x01};
-/* A binary value generated from the base64 decoding of q3.message.query_time_sec */
-const char a3_query_time_sec[] = {0x8e,0x04,0x50,0x64,0x00,0x00,0x00,0x00};
-/* A binary value generated from the base64 decoding of q3.message.query_time_nsec */
-const char a3_query_time_nsec[] = {0xc0,0xde,0x76,0x08};
 
-const answer_t a1[] = {{"type", 7, "unknown"},
-		       {"dstip", 9, "192.0.2.2"},
-		       {"dstport", 2, "80"},
-		       {"request", 6, "GET /\0"},
-		       { NULL, 0, NULL}};
+#define A1_MESSAGE	QUOTE({"time":"2018-02-20 22:01:47.303896708","vname":"base","mname":"http","source":"abcdef01","message":{"type":"unknown","dstip":"192.0.2.2","dstport":80,"request":"GET /"}})
 
-const answer_t a2[] = {{"time", 10, "1682087949"},
-		       {"response_ip", 3, "::1"},
-		       {"qname", 16, "xxx.xxx.xxx.xxx."},
-		       {"qtype", 1, "A"},
-		       {"qclass", 2, "IN"},
-		       {"rcode", 7, "NOERROR"},
-		       {"response", sizeof(a2_response), a2_response},
-		       {"response_json", 615, QUOTE({"header":{"opcode":"QUERY","rcode":"NOERROR","id":23444,"flags":["qr","aa","ad"],"opt":{"edns":{"version":0,"flags":["do"],"udp":4096,"options":[]}}},"question":[{"qname":"xxxx.xxxx.xxxxx.xxx.xxx.xxxxxxxxxxx.xxx.","qclass":"IN","qtype":"A"}],"answer":[{"rrname":"xxxx.xxxx.xxxxx.xxx.xxx.xxxxxxxxxxx.xxx.","rrttl":60,"rrclass":"IN","rrtype":"A","rdata":["0.0.0.0","0.0.0.0","0.0.0.0"]}],"authority":[{"rrname":"xxxxx.xxx.xxx.xxxxxxxxxxx.xxx.","rrttl":172800,"rrclass":"IN","rrtype":"NS","rdata":["xxxxxxx.xxxxxxxxx.xxx.","xxxxxxx.xxxxxxxxx.xx.xx.","xxxxxx.xxxxxxxxx.xxx.","xxxxxx.xxxxxxxxx.xxx."]}],"additional":[]})},
-		       {"query_zone", 16, "xxx.xxx.xxx.xxx."},
-		       {"geoid", 1, "\x0"},
-		       {"sensor_id", 1, "4"},
-		       { NULL, 0, NULL}};
+/* Type base:http */
+const answer_t a1[] = {
+	{ "type", 7, "unknown" },	/* enum */
+	{ "dstip", 9, "192.0.2.2" },	/* bytes | nmsg_msgmod_ft_ip */
+	{ "dstport", 2, "80" },		/* uint32 */
+	{ "request", 6, "GET /\0"},	/* bytes | nmsg_msgmod_ft_mlstring */
+	{ NULL, 0, NULL }
+};
 
-const answer_t a3[] = {{"type", 20, "UDP_UNANSWERED_QUERY"},
-		       {"query_ip", 7, "0.0.0.0"},
-		       {"response_ip", 7, "0.0.0.0"},
-		       {"proto", 3, "UDP"},
-		       {"query_port", 4, "5353"},
-		       {"response_port", 4, "5353"},
-		       {"id", 1, "0"},
-		       {"qname", 26, "_microsoft_mcc._tcp.local."},
-		       {"qclass", 10, "CLASS32769"},
-		       {"qtype", 7, "TYPE149"},
-		       {"query_packet", sizeof(a3_query_packet), a3_query_packet},
-		       {"query_time_sec", sizeof(a3_query_time_sec), a3_query_time_sec},
-		       {"query_time_nsec", sizeof(a3_query_time_nsec), a3_query_time_nsec},
-		       {"response_packet", 0, "\0"},
-		       {"response_time_sec", 0, "\0"},
-		       {"response_time_nsec", 0, "\0"},
-		       {"timeout", 21, "72.502578999999997222"},
-		       {"query", sizeof(a3_query), a3_query},
-		       {"query_json", 196, QUOTE({"header":{"opcode":"QUERY","rcode":"NOERROR","id":0,"flags":[]},"question":[{"qname":"_microsoft_mcc._tcp.local.","qclass":"CLASS32769","qtype":"PTR"}],"answer":[],"authority":[],"additional":[]})},
-		       { NULL, 0, NULL}};
+#define A2_MESSAGE	QUOTE({"time":"2023-04-21 14:39:10.039412373","vname":"base","mname":"dnsobs","message":{"time":1682087949,"response_ip":"::1","qname":"xxx.xxx.xxx.xxx.","qtype":"A","qclass":"IN","rcode":"NOERROR","response":"W5SEIAABAAMABAABBHh4eHgEeHh4eAV4eHh4eAN4eHgDeHh4C3h4eHh4eHh4eHh4A3h4eAAAAQABwAwAAQABAAAAPAAEAAAAAMAMAAEAAQAAADwABAAAAADADAABAAEAAAA8AAQAAAAAwBYAAgABAAKjAAAXB3h4eHh4eHgJeHh4eHh4eHh4A3h4eADAFgACAAEAAqMAABkHeHh4eHh4eAl4eHh4eHh4eHgCeHgCeHgAwBYAAgABAAKjAAATBnh4eHh4eAl4eHh4eHh4eHjAMMAWAAIAAQACowAAFgZ4eHh4eHgJeHh4eHh4eHh4A3h4eAAAACkQAAAAgAAAAA==","response_json":{"header":{"opcode":"QUERY","rcode":"NOERROR","id":23444,"flags":["qr","aa","ad"],"opt":{"edns":{"version":0,"flags":["do"],"udp":4096,"options":[]}}},"question":[{"qname":"xxxx.xxxx.xxxxx.xxx.xxx.xxxxxxxxxxx.xxx.","qclass":"IN","qtype":"A"}],"answer":[{"rrname":"xxxx.xxxx.xxxxx.xxx.xxx.xxxxxxxxxxx.xxx.","rrttl":60,"rrclass":"IN","rrtype":"A","rdata":["0.0.0.0","0.0.0.0","0.0.0.0"]}],"authority":[{"rrname":"xxxxx.xxx.xxx.xxxxxxxxxxx.xxx.","rrttl":172800,"rrclass":"IN","rrtype":"NS","rdata":["xxxxxxx.xxxxxxxxx.xxx.","xxxxxxx.xxxxxxxxx.xx.xx.","xxxxxx.xxxxxxxxx.xxx.","xxxxxx.xxxxxxxxx.xxx."]}],"additional":[]},"query_zone":"xxx.xxx.xxx.xxx.","geoid":"ESIzRA==","sensor_id":"423a35c7"}})
 
-const task_t tasks[] = {{QUOTE({"time":"2018-02-20 22:01:47.303896708","vname":"base","mname":"http","source":"abcdef01","message":{"type":"unknown","dstip":"192.0.2.2","dstport":80,"request":"GET /"}}),
-			a1},
-			{QUOTE({"time":"2023-04-21 14:39:10.039412373","vname":"base","mname":"dnsobs","message":{"time":1682087949,"response_ip":"::1","qname":"xxx.xxx.xxx.xxx.","qtype":"A","qclass":"IN","rcode":"NOERROR","response":"W5SEIAABAAMABAABBHh4eHgEeHh4eAV4eHh4eAN4eHgDeHh4C3h4eHh4eHh4eHh4A3h4eAAAAQABwAwAAQABAAAAPAAEAAAAAMAMAAEAAQAAADwABAAAAADADAABAAEAAAA8AAQAAAAAwBYAAgABAAKjAAAXB3h4eHh4eHgJeHh4eHh4eHh4A3h4eADAFgACAAEAAqMAABkHeHh4eHh4eAl4eHh4eHh4eHgCeHgCeHgAwBYAAgABAAKjAAATBnh4eHh4eAl4eHh4eHh4eHjAMMAWAAIAAQACowAAFgZ4eHh4eHgJeHh4eHh4eHh4A3h4eAAAACkQAAAAgAAAAA==","response_json":{"header":{"opcode":"QUERY","rcode":"NOERROR","id":23444,"flags":["qr","aa","ad"],"opt":{"edns":{"version":0,"flags":["do"],"udp":4096,"options":[]}}},"question":[{"qname":"xxxx.xxxx.xxxxx.xxx.xxx.xxxxxxxxxxx.xxx.","qclass":"IN","qtype":"A"}],"answer":[{"rrname":"xxxx.xxxx.xxxxx.xxx.xxx.xxxxxxxxxxx.xxx.","rrttl":60,"rrclass":"IN","rrtype":"A","rdata":["0.0.0.0","0.0.0.0","0.0.0.0"]}],"authority":[{"rrname":"xxxxx.xxx.xxx.xxxxxxxxxxx.xxx.","rrttl":172800,"rrclass":"IN","rrtype":"NS","rdata":["xxxxxxx.xxxxxxxxx.xxx.","xxxxxxx.xxxxxxxxx.xx.xx.","xxxxxx.xxxxxxxxx.xxx.","xxxxxx.xxxxxxxxx.xxx."]}],"additional":[]},"query_zone":"xxx.xxx.xxx.xxx.","geoid":"AA==","sensor_id":"423a35c7"}}),
-			 a2},
-			{QUOTE({"time":"2023-05-01 18:27:26.142008000","vname":"base","mname":"dnsqr","message":{"type":"UDP_UNANSWERED_QUERY","query_ip":"0.0.0.0","response_ip":"0.0.0.0","proto":"UDP","query_port":5353,"response_port":5353,"id":0,"qname":"_microsoft_mcc._tcp.local.","qclass":"CLASS32769","qtype":"TYPE149","query_packet":["RQAARz7IAAABEa3DrB5AAeAAAPsU6RTpADNfHQAAAAAAAQAAAAAAAA5fbWljcm9zb2Z0X21jYwRfdGNwBWxvY2FsAAAMgAE="],"query_time_sec":[1682965646],"query_time_nsec":[142008000],"response_packet":[],"response_time_sec":[],"response_time_nsec":[],"timeout":72.502578999999997222,"query":"AAAAAAABAAAAAAAADl9taWNyb3NvZnRfbWNjBF90Y3AFbG9jYWwAAAyAAQ==","query_json":{"header":{"opcode":"QUERY","rcode":"NOERROR","id":0,"flags":[]},"question":[{"qname":"_microsoft_mcc._tcp.local.","qclass":"CLASS32769","qtype":"PTR"}],"answer":[],"authority":[],"additional":[]}}}),
-			 a3},
-			{NULL,NULL}};
+#define A3_MESSAGE	QUOTE({"time":"2023-05-01 18:27:26.142008000","vname":"base","mname":"dnsqr","message":{"type":"UDP_UNANSWERED_QUERY","query_ip":"0.0.0.0","response_ip":"0.0.0.0","proto":"UDP","query_port":5353,"response_port":5353,"id":0,"qname":"_microsoft_mcc._tcp.local.","qclass":"CLASS32769","qtype":"TYPE149","query_packet":["RQAARz7IAAABEa3DrB5AAeAAAPsU6RTpADNfHQAAAAAAAQAAAAAAAA5fbWljcm9zb2Z0X21jYwRfdGNwBWxvY2FsAAAMgAE="],"query_time_sec":[1682965646],"query_time_nsec":[142008000],"response_packet":[],"response_time_sec":[],"response_time_nsec":[],"timeout":72.502578999999997222,"query":"AAAAAAABAAAAAAAADl9taWNyb3NvZnRfbWNjBF90Y3AFbG9jYWwAAAyAAQ==","query_json":{"header":{"opcode":"QUERY","rcode":"NOERROR","id":0,"flags":[]},"question":[{"qname":"_microsoft_mcc._tcp.local.","qclass":"CLASS32769","qtype":"PTR"}],"answer":[],"authority":[],"additional":[]}}})
+
+/* Type base:dnsobs */
+const answer_t a2[] = {
+	{ "time", 10, "1682087949" },	/* uint64 */
+	{ "response_ip", 3, "::1" },	/* bytes | nmsg_msgmod_ft_ip */
+	{ "qname", 16, "xxx.xxx.xxx.xxx." },	/* bytes | dns_name_format */
+	{ "qtype", 1, "A" },		/* uint32 | dns_type_format */
+	{ "qclass", 2, "IN" },		/* uint32 | dns_class_format */
+	{ "rcode", 7, "NOERROR" },	/* uint32 i| dnsqr_rcode_format */
+	{ "query_zone", 16, "xxx.xxx.xxx.xxx." },	/* bytes | dns_name_format */
+	{ "geoid", 4, "\x11\x22\x33\x44" },		/* bytes */
+	{ "sensor_id", 1, "4" },	/* fixed32 | dnsobs_sid_format */
+	{ NULL, 0, NULL }
+};
+
+/* Type base:dnsqr */
+const answer_t a3[] = {
+	{ "type", 20, "UDP_UNANSWERED_QUERY" },	/* enum */
+	{ "query_ip", 7, "0.0.0.0" },		/* bytes | nmsg_msgmod_ft_ip */
+	{ "response_ip", 7, "0.0.0.0" },	/* bytes | nmsg_msgmod_ft_ip */
+	{ "proto", 3, "UDP" },		/* uint32 | dnsqr_proto_format */
+	{ "query_port", 4, "5353" },	/* uint32 */
+	{ "response_port", 4, "5353" },	/* uint32 */
+	{ "id", 1, "0" },		/* uint32 */
+	{ "qname", 26, "_microsoft_mcc._tcp.local." },	/* bytes | dns_name_format */
+	{ "qclass", 10, "CLASS32769" },	/* uint32 | dns_class_format */
+	{ "qtype", 7, "TYPE149" },	/* uint32 | dns_type_format */
+	{ "query_time_sec", 8, "\x8e\x04\x50\x64\x00\x00\x00\x00" },	/* int64 */
+	{ "query_time_nsec", 4, "\xc0\xde\x76\x08" },	/* sfixed32: 142008000 */
+	{ "response_time_sec", 0, "\0" },	/* int64 */
+	{ "response_time_nsec", 0, "\0" },	/* sfixed32 */
+	{ "timeout", 21, "72.502578999999997222" },	/* double */
+	{ NULL, 0, NULL }
+};
+
+const task_t tasks[] = {
+	{ A1_MESSAGE, a1 },
+	{ A2_MESSAGE, a2 },
+	{ A3_MESSAGE, a3 },
+	{NULL,NULL}
+};
 
 static int
 test_kafka_key(void) {
@@ -119,8 +117,8 @@ test_kafka_key(void) {
 	check_return(fd != -1);
 
 	t = tasks;
-	while (t->question != NULL) {
-		check_return(write(fd, t->question, strlen(t->question)) == (ssize_t) strlen(t->question));
+	while (t->payload != NULL) {
+		check_return(write(fd, t->payload, strlen(t->payload)) == (ssize_t) strlen(t->payload));
 		check_return(write(fd, "\n", 1) == 1);
 		++t;
 	}
@@ -131,10 +129,10 @@ test_kafka_key(void) {
 	check_return(i != NULL);
 
 	t = tasks;
-	while (t->question != NULL) {
+	while (t->payload != NULL) {
 		check_return(nmsg_input_read(i, &m) == nmsg_res_success);
 		a = t->answer;
-		printf("Question [%s]\n",t->question);
+		printf("Question [%s]\n",t->payload);
 		while (a->field != NULL) {
 			check_return(_nmsg_message_payload_get_field_value_as_key(m, a->field, tb) == nmsg_res_success);
 			printf("Key [%s] Expect [%s][%lu] - Result [%s][%lu]\n", a->field, a->response, a->length, tb->data, nmsg_strbuf_len(tb));
